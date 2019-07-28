115327d8e47f2316fdb7d7d65ef163f39360690d
GeorgeChryso <lsgechrys@gmail.com>
2019-07-29 02:18:55 +0300

    First Missing Positive

---
+++ 41. First Missing Positive.js
@@ -0,0 +1,32 @@
+// Given an unsorted integer array, find the smallest missing positive integer.
+
+// Example 1:
+
+// Input: [1,2,0]
+// Output: 3
+
+var firstMissingPositive = function(nums) {
+    let z=  Math.max(... nums.filter(
+    (d)=>d>0?d:0));
+      
+    for (var i=1; i<=z; i++){
+        
+      if  (nums.includes(i)){
+        
+      } 
+       else{
+         break;
+       
+       }
+        
+    } 
+      
+        return i
+        
+    };
+
+console.log(
+    firstMissingPositive(
+        [1,2,0]
+    )
+)
