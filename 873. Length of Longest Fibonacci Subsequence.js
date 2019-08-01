473b417b086e9cdb4f0216b4750f5ba018a804e2
GeorgeChryso <lsgechrys@gmail.com>
2019-08-01 21:27:32 +0300

    Fib

---
+++ 873. Length of Longest Fibonacci Subsequence.js
@@ -0,0 +1,27 @@
+// A sequence X_1, X_2, ..., X_n is fibonacci-like if:
+
+//  n >= 3
+
+// X_i + X_{i+1} = X_{i+2} for all i + 2 <= n
+
+
+// Given a strictly increasing array A of positive integers forming a sequence, find the length of the longest fibonacci-like subsequence of A.  If one does not exist, return 0.
+
+// (Recall that a subsequence is derived from another sequence A by deleting any number of elements (including none) from A, without changing the order of the remaining elements.  For example, [3, 5, 8] is a subsequence of [3, 4, 5, 6, 7, 8].)
+
+// Example 1:
+
+// Input: [1,2,3,4,5,6,7,8]
+// Output: 5
+// Explanation:
+// The longest subsequence that is fibonacci-like: [1,2,3,5,8]
+
+var lenLongestFibSubseq = function(A) {
+    let c=0
+    let Fibs=[
+    ]
+    A.forEach((d,i)=>{
+        
+    })
+
+};
