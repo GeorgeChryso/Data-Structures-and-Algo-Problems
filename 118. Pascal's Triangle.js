936ee99dbd04187590bf42498cec422339e93ab5
GeorgeChryso <lsgechrys@gmail.com>
2019-08-04 09:08:21 +0300

    118

--- 118. Pascal's Triangle.js
+++ 118. Pascal's Triangle.js
@@ -1,23 +1,29 @@
 // Given a non-negative integer numRows, generate the first numRows of Pascal's triangle.
 
+
+
+
 var generate = function(x) {
     if(x==0){return []}
     let A=[[1]]
     for (let k = 1; k < x; k++) {
-        let z=[]
+        A.push([])
         for (let i = 0; i <A[k-1].length+1; i++) {
             
-                 (i==0||i==A[k-1].length)?z.push(1):
-                z.push( A[k-1][i-1]+A[k-1][i]);
+                 (i==0||i==A[k-1].length)?A[k].push(1):
+                A[k].push( A[k-1][i-1]+A[k-1][i]);
             
             
         }
-        A.push(z)
 
     }
     return A
 };
 
+
+
+
+
 console.log(
-    generate(3)
+    generate1(6)
 )
