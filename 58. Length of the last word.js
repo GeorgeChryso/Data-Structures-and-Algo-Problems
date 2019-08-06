b0bec575ed9d7972d80f8e041c02993b6f965454
GeorgeChryso <lsgechrys@gmail.com>
2019-08-06 23:16:07 +0300

    58 added 2 solutions

--- 58. Length of the last word.js
+++ 58. Length of the last word.js
@@ -31,8 +31,21 @@ else{
 var lengthOfLastWord = function(s) {
     return s.trim().split(" ").pop().length;
 };/// poor speed
+//trim removes all whitespace in the end and the beginning of the string, split creates an array and pop returns the last element of the array
 
-var z=( )+g
+
+var lengthOfLastWord = function(s) {
+    var endPointer = s.length - 1; // parses the string
+    
+    while(s[endPointer] === ' '){ // until the first non whitespace character
+        endPointer--;
+    }
+    var arr = s.substring(0,endPointer + 1).split(' '); // then creates an array splitting on whitespace and returns the last element's length
+    return arr[arr.length - 1].length;
+};
+
+
+
 console.log('da da'.indexOf())
 
 // console.log(lol(
