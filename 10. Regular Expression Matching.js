830f98e93cfc4a038a10df1b2a99e8a4ffffe6d4
GeorgeChryso <lsgechrys@gmail.com>
2019-08-10 13:07:29 +0300

    10 regex tricky easy

---
+++ 10. Regular Expression Matching.js
@@ -0,0 +1,43 @@
+// Given an input string (s) and a pattern (p), implement regular expression matching with support for '.' and '*'.
+
+// '.' Matches any single character.
+// '*' Matches zero or more of the preceding element.
+// The matching should cover the entire input string (not partial).
+
+// Note:
+
+// s could be empty and contains only lowercase letters a-z.
+// p could be empty and contains only lowercase letters a-z, and characters like . or *.
+// Example 1:
+
+// Input:
+// s = "aa"
+// p = "a"
+// Output: false
+// Explanation: "a" does not match the entire string "aa".
+// Example 2:
+
+// Input:
+// s = "aa"
+// p = "a*"
+// Output: true
+// Explanation: '*' means zero or more of the preceding element, 'a'. Therefore, by repeating 'a' once, it becomes "aa".
+
+
+var isMatch = function(s, p) {
+    return s.match(new RegExp(p))==s
+};
+
+
+// new RegExp(..) 
+// The RegExp constructor creates a regular expression object
+// for matching text with a pattern.
+
+
+
+
+console.log(
+    isMatch(
+        "aa",'a*' 
+    )
+)
