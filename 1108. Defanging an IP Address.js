2137ad714c99ccde819fcd658681d4e214850048
GeorgeChryso <lsgechrys@gmail.com>
2019-08-15 21:54:35 +0300

    1108

---
+++ 1108. Defanging an IP Address.js
@@ -0,0 +1,12 @@
+// Given a valid(IPv4) IP address, return a defanged version of that IP address.
+
+// A defanged IP address replaces every period "." with "[.]".
+
+var defangIPaddr = function (address) {
+    return address.replace(/\./g, '\[.\]')
+};
+
+
+console.log(defangIPaddr(
+    "255.100.50.0"
+))
