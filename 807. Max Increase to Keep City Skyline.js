61e69ad86d28baedb8fa17b6a7b1905d05389fcf
GeorgeChryso <lsgechrys@gmail.com>
2019-08-16 22:30:19 +0300

    535 Optimized..

--- 807. Max Increase to Keep City Skyline.js
+++ 807. Max Increase to Keep City Skyline.js
@@ -54,9 +54,12 @@ var maxIncreaseKeepingSkyline = function (grid) {
 console.log(
     maxIncreaseKeepingSkyline(
          
-        [[3, 0, 8, 4],
+        [
+            [3, 0, 8, 4],
             [2, 4, 5, 7],
             [9, 2, 6, 3],
-            [0, 3, 1, 0]]
+            [0, 3, 1, 0]
+        ]
+        
     )
 )
