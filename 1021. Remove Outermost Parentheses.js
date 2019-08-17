2d598ece71a8e6a184b4280fc25d84f055385441
GeorgeChryso <lsgechrys@gmail.com>
2019-08-17 19:10:41 +0300

    804 Optimized

--- 1021. Remove Outermost Parentheses.js
+++ 1021. Remove Outermost Parentheses.js
@@ -9,33 +9,20 @@
 var removeOuterParentheses = function (S) {
     let c = 0
     let arr = ''
-    let k=1
+    let k =''
     for (let i = 0; i < S.length;i++) {
-        if (S[i]=='(') {
-            c++
+        c+=S[i]=='('?1:-1
+        k+=S[i]
+
+     if ( c == 0 ){
+            arr=arr + k.slice(1, -1)
+            k=''
             
-        }
-        else {
-            c--
-        }
-     
-
-        if (c == 0) {
-            console.log(i)
-            console.log(i == S.length-1)
-
-            S = S.slice(0, i ) + S.slice(i + 1, S.length )
-            c--
-            i++
-            // arr+=S.slice(k, i)        
-            // k = i + 2
-           // if (i == S.length - 1) { return S.slice(1, S.length) }
         }
             
         
         
     }
-    return S.slice(1, S.length)
     return arr
 
 };
---
+++ 804. Unique Morse Code Words.js
@@ -0,0 +1,35 @@
+// nternational Morse Code defines a standard encoding where each letter is mapped to a series of dots and dashes, as follows: "a" maps to ".-", "b" maps to "-...", "c" maps to "-.-.", and so on.
+
+// For convenience, the full table for the 26 letters of the English alphabet is given below:
+
+// Now, given a list of words, each word can be written as a concatenation of the Morse code of each letter.For example, "cba" can be written as "-.-..--...", (which is the concatenation "-.-." + "-..." + ".-").We'll call such a concatenation, the transformation of a word.
+
+// Return the number of different transformations among all words we have.
+
+//     Example:
+// Input: words = ["gin", "zen", "gig", "msg"]
+// Output: 2
+// Explanation:
+// The transformation of each word is:
+// "gin" -> "--...-."
+// "zen" -> "--...-."
+// "gig" -> "--...--."
+// "msg" -> "--...--."
+
+// There are 2 different transformations, "--...-." and "--...--.".
+
+
+var table= [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."]
+var z='abcdefghijklmnopqrstuvwxyz'
+var uniqueMorseRepresentations = function (words) {
+    function transform(x) {
+     return x.split('').map((d)=>table[z.indexOf(d)]).join('')
+        
+    }
+    let  S= new Set()
+    words.forEach((d)=>S.add(transform(d)))
+return S.size
+};
+console.log(uniqueMorseRepresentations(
+    ["gin", "zen", "gig", "msg"]
+))
