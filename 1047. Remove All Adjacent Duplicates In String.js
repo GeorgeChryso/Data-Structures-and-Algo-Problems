c5c8fb27c6379c2316bf9f8861738436248ca313
GeorgeChryso <lsgechrys@gmail.com>
2019-08-18 23:52:58 +0300

    1047 Space opt

---
+++ 1047. Remove All Adjacent Duplicates In String.js
@@ -0,0 +1,23 @@
+// Given a string S of lowercase letters, a duplicate removal consists of choosing two adjacent and equal letters, and removing them.
+
+// We repeatedly make duplicate removals on S until we no longer can.
+
+// Return the final string after all such duplicate removals have been made.  It is guaranteed the answer is unique.
+
+var removeDuplicates = function(S) {
+  
+
+   // while([...S.match((/(.)\1+/))].length!=0){}
+ while(/(.)\1/.test(S)){
+S= S.replace(/(.)\1/,'')  }
+return  S
+
+};
+
+console.log(
+    removeDuplicates(
+        "abbaca"
+    )
+)
+
+// /(.)\1+/.test(  "abbaca")
