47f2e875efa92c83a3447329ba42c1b44e953710
GeorgeChryso <lsgechrys@gmail.com>
2019-08-20 12:56:43 +0300

    Unopt 986

--- 986. Interval List Intersections.js
+++ 986. Interval List Intersections.js
@@ -8,7 +8,8 @@ var intervalIntersection = (function(A, B) {
     let answ=[]
     for (let i = 0; i < A.length; i++) {
         for (let j = 0; j < B.length; j++) {
-            answ.push(intersect(A[i],B[j]!=0?intersect(A[i],B[j]):null)          )            
+
+            (intersect(A[i],B[j])!=0?answ.push(intersect(A[i],B[j])):null)                 
         }
    
 
@@ -19,8 +20,9 @@ var intervalIntersection = (function(A, B) {
         if(A[0]<B[0]){
             if(A[1]<B[0]){return 0}
             else if ( A[1]<B[1]){return [B[0],A[1]]}
-            else if (A[1]>B[0]){return [B[0],A[1]]}
             else if (A[1]>B[1]){return [B[0],B[1]]}
+
+            else if (A[1]>B[0]){return [B[0],A[1]]}
             else if (A[1]==B[0]){
                 return [B[0],B[0]]
             }
@@ -53,7 +55,9 @@ var intervalIntersection = (function(A, B) {
     }
     return answ
 })(
-    [[0,2],[5,10],[13,23],[24,25]],[[1,5],[8,12],[15,24],[25,26]]
+    [[3,5],[9,20]],
+    [[4,5],[7,10],[11,12],[14,15],[16,20]]
+
 )
 
 console.log(intervalIntersection)
