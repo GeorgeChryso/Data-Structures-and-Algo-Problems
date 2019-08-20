6fa4cc50f5d4fa0396459f72911e9d7d48604fae
GeorgeChryso <lsgechrys@gmail.com>
2019-08-20 13:14:27 +0300

    Unopt 986 Progress

--- 986. Interval List Intersections.js
+++ 986. Interval List Intersections.js
@@ -8,8 +8,9 @@ var intervalIntersection = (function(A, B) {
     let answ=[]
     for (let i = 0; i < A.length; i++) {
         for (let j = 0; j < B.length; j++) {
-
-            (intersect(A[i],B[j])!=0?answ.push(intersect(A[i],B[j])):null)                 
+            if(A[i][1]>=B[j][0])
+            {console.log('aa')
+                intersect(A[i],B[j])  }
         }
    
 
@@ -19,35 +20,27 @@ var intervalIntersection = (function(A, B) {
     function intersect(A,B){
         if(A[0]<B[0]){
             if(A[1]<B[0]){return 0}
-            else if ( A[1]<B[1]){return [B[0],A[1]]}
-            else if (A[1]>B[1]){return [B[0],B[1]]}
-
-            else if (A[1]>B[0]){return [B[0],A[1]]}
-            else if (A[1]==B[0]){
-                return [B[0],B[0]]
-            }
-            else if (A[1]==B[1]){
-                return [B[0],A[1]]
-            }
+            else if ( A[1]<=B[1]){answ.push([B[0],A[1]])}
+            else if (A[1]>B[1]){ answ.push([B[0],B[1]])}
+            
         }
         else if ( A[0]==B[0]){ 
-            if( A[1]>B[1]){ return [A[0],B[1]]}
+            if( A[1]>B[1]){  answ.push([A[0],B[1]]) }
             else {
-                return [A[0],A[1]]
+                 answ.push([A[0],A[1]])
             }
         }
         else {
-            if( A[0]>B[1]){ return 0}
+            if( A[0]>B[1]){return 0}
             else if ( A[0]<B[1]){
-                if( A[1]<B[1]){ return [A[0],A[1]]}
-                else if (A[1]==B[1]){ return [A[0],A[1]]}
+                if( A[1]<=B[1]){  answ.push([A[0],A[1]])}
                 else{ 
-                    return [A[0],B[1]]
+                    answ.push([A[0],B[1]])
                 }
 
             }
             else{
-                return [A[0],A[0]]
+                answ.push([A[0],A[0]])
             }
 
         }
@@ -55,8 +48,8 @@ var intervalIntersection = (function(A, B) {
     }
     return answ
 })(
-    [[3,5],[9,20]],
-    [[4,5],[7,10],[11,12],[14,15],[16,20]]
+    [[1,7]],
+    [[3,10]]
 
 )
 
