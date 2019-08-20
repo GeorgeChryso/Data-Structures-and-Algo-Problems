31b2e99e620184ee11d06dec14cf08cf3da4207a
GeorgeChryso <lsgechrys@gmail.com>
2019-08-20 14:12:01 +0300

    912 otpimzied,needs followup recursion

---
+++ 912.! Sort an Array.js
@@ -0,0 +1,33 @@
+var sortArray = function(nums) {
+    return  nums.sort((a,b)=>a-b)
+  };
+
+var sortArray = function(nums) {
+   
+ 
+};
+
+function merge(left, right) {
+	const result = [];
+	while (left.length && right.length) {
+		if (left[0] < right[0]) {
+			result.push(left.shift());
+		} else {
+			result.push(right.shift());
+		}
+	}
+	return result.concat(...left, ...right);
+}
+
+var sortArray = function(nums) {
+	if (nums.length < 2) {
+		return nums;
+	} else {
+		const m = Math.floor(nums.length / 2);
+		const left = sortArray(nums.slice(0, m));
+		const right = sortArray(nums.slice(m));
+		return merge(left, right);
+	}
+};
+
+// Read up on that recursion
