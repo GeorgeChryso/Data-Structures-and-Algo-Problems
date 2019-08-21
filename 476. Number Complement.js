5f31589b78951c8b16c3928d6b731a1b47ede177
GeorgeChryso <lsgechrys@gmail.com>
2019-08-21 20:49:46 +0300

    476 solved

---
+++ 476. Number Complement.js
@@ -0,0 +1,18 @@
+// Given a positive integer, output its complement number. The complement strategy is to flip the bits of its binary representation.
+
+// Note:
+// The given integer is guaranteed to fit within the range of a 32-bit signed integer.
+// You could assume no leading zero bit in the integer’s binary representation.
+
+
+var findComplement =( function(num) {
+  //  return parseInt(num.toString(2).split('').map((d)=>d==0?1:0).join(''),2)
+  // return String(num).replace(/1|0/g,)
+}
+)(20161211)
+
+
+
+
+console.log(findComplement)
+
---
+++ 959. Regions Cut By Slashes.js
@@ -0,0 +1,29 @@
+// In a N x N grid composed of 1 x 1 squares, each 1 x 1 square consists of a /, \, or blank space.  These characters divide the square into contiguous regions.
+
+// (Note that backslash characters are escaped, so a \ is represented as "\\".)
+
+// Return the number of regions.
+
+
+var regionsBySlashes = (function(A) {
+    let c=0
+
+    for (let i = 0; i < A.length; i++) {
+      for (let j = 0; j < A[0].length; j++) {
+          
+      }      
+    }
+
+
+
+    
+}
+)
+
+([
+    "/\\",
+    "\\/"
+  ])
+
+
+  console.log(regionsBySlashes)
