1dfb65c5acd69fdda771de7a4cb1f4cf552de13d
GeorgeChryso <lsgechrys@gmail.com>
2019-08-21 20:55:13 +0300

    476 optimized

--- 476. Number Complement.js
+++ 476. Number Complement.js
@@ -7,9 +7,11 @@
 
 var findComplement =( function(num) {
   //  return parseInt(num.toString(2).split('').map((d)=>d==0?1:0).join(''),2)
-  // return String(num).replace(/1|0/g,)
+
+
+   return parseInt(String(num.toString(2)).replace(/1|0/g,(x)=>x==1?'0':1),2)
 }
-)(20161211)
+)(5)
 
 
 
