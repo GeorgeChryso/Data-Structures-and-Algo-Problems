fe0d80808d3432e8f41c2a9ed5d348acd50f7bb0
GeorgeChryso <lsgechrys@gmail.com>
2019-08-21 21:03:15 +0300

    478 neat trick

--- 476. Number Complement.js
+++ 476. Number Complement.js
@@ -9,12 +9,25 @@ var findComplement =( function(num) {
   //  return parseInt(num.toString(2).split('').map((d)=>d==0?1:0).join(''),2)
 
 
-   return parseInt(String(num.toString(2)).replace(/1|0/g,(x)=>x==1?'0':1),2)
+   return parseInt(num.toString(2).replace(/1|0/g,(x)=>x==1?'0':1),2)
 }
 )(5)
 
+var findComplement = function(num) {
+    if (num === 1) return 0
+    let i = 1;
+    while (i <= num) {
+      i *= 2;
+    }
+    
+    return i - 1 - num;
+    
+  };
 
 
-
-console.log(findComplement)
+// Any complement of a number is X equal to 
+// where 2^A is the closest >= integer of X
+// 2^A>X  2^A-X-1
+// NEAT TRICKU
+console.log(findComplement(5))
 
