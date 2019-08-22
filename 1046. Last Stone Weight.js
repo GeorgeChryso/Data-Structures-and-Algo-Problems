4ede74b40d3e02fb8e0e2bebe8cba7edc108ece6
GeorgeChryso <lsgechrys@gmail.com>
2019-08-23 00:13:58 +0300

    1046 progress

--- 1046. Last Stone Weight.js
+++ 1046. Last Stone Weight.js
@@ -2,21 +2,23 @@
 
 
 var lastStoneWeight = function(A) {
-    while(Math.max(...A)>1&&A.length>1){
+    while(A.length>1){
              A= A.sort((a,b)=>a-b)
-        if( A[A.length-2]==A[A.length-1]){
-                A.splice(A[A.length-2],2)
+                let len=A.length    
+        if( A[len-2]==A[len-1]){
+                A.splice(len-2,2)
+            
         }
         else{
-            A[A.length-1]=A[A.length-1]-A[A.length-2]
-            A.splice(A[A.length-2],1)
-
+            A[len-1]=A[len-1]-A[len-2]
+              A.splice(len-2,1)
         }
-        console.log(A)
     }
-    return A[0]
+    return A[0]==undefined?0:A[0]
 };
 
 console.log(lastStoneWeight(
-    [1,3]
-    ))
+   
+
+    [9,3,2,10]
+        ))
