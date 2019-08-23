c24562e91426a00a334e552a826c314d54106e4e
GeorgeChryso <lsgechrys@gmail.com>
2019-08-23 19:34:53 +0300

    1046 optimized

--- 1046. Last Stone Weight.js
+++ 1046. Last Stone Weight.js
@@ -17,6 +17,30 @@ var lastStoneWeight = function(A) {
     return A[0]==undefined?0:A[0]
 };
 
+var lastStoneWeight = function (stones) {
+    stones.sort((a, b) => a - b);
+    while (stones.length > 1) {
+        let a = stones.pop();
+        let b = stones.pop();
+        let diff = a > b ? a - b : b - a;
+        if (stones.length === 0) {
+            stones.unshift(diff);
+        } else if (diff > 0) {
+            let i = 0;
+            for (; i < stones.length; i++) {
+                if (stones[i] > diff) {
+                    stones.splice(i, 0, diff)
+                    break;
+                }
+            }
+            if (i >= stones.length) {
+                stones.push(diff);
+            }
+        }
+    }
+    return stones[0];
+};
+
 console.log(lastStoneWeight(
    
 
