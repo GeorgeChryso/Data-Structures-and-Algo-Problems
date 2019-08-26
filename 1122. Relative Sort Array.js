21a6fc025b4354a14849eb544fa61ecfbccc651e
GeorgeChryso <lsgechrys@gmail.com>
2019-08-26 22:27:21 +0300

    1122 optimized

--- 1122. Relative Sort Array.js
+++ 1122. Relative Sort Array.js
@@ -18,49 +18,89 @@
 
 
 var relativeSortArray = function(arr1, arr2) {
-    let rem=[]
-    let l=arr2.length+1-1
-    
+    let rem=new Set()
+   // let l=arr2.length+1-1
+    let objy={}
+    arr2.forEach((d,i)=>objy[d]=i)
 
-        arr2=arr2.map((d,i)=>[d,i])
-        for (let i = 0; i < l; i++) {
-            if ( arr2[i][0]==undefined){
-                continue;
-            }
-            if(arr2[arr2[i][0]]!=undefined){
-                if(arr2[arr2[i][0]][0]==arr2[i][0]){ continue}
-                let temp=arr2[arr2[i][0]]
-                arr2[arr2[i][0]]=arr2[i]
-                arr2[i]=temp
-                i--
+        // arr2=arr2.map((d,i)=>[d,i])
+        // for (let i = 0; i < l; i++) {
+        //     if ( arr2[i][0]==undefined){
+        //         continue;
+        //     }
+        //     if(arr2[arr2[i][0]]!=undefined){
+        //         if(arr2[arr2[i][0]][0]==arr2[i][0]){ continue}
+        //         let temp=arr2[arr2[i][0]]
+        //         arr2[arr2[i][0]]=arr2[i]
+        //         arr2[i]=temp
+        //         i--
                 
-            }
-            else{
-                arr2[arr2[i][0]]=arr2[i]
-                arr2[i]=undefined
-            }
+        //     }
+        //     else{
+        //         arr2[arr2[i][0]]=arr2[i]
+        //         arr2[i]=undefined
+        //     }
       
-            console.log (arr2)
+        //     console.log (arr2)
 
-        }
+        // }
 
-    arr1=arr1.filter((a)=>{
-        if(arr2[a]){
-            return true}
-        else {
-                          rem.push(a)
-                          return false;
-                      }})
+    // arr1=arr1.filter((a)=>{
+    //     if(arr2[a]){
+    //         return true}
+    //     else {
+    //                       rem.push(a)
+    //                       return false;
+    //                   }})
 
         
-    arr1.sort((a,b)=>{
-        if (arr2[a][1]<arr2[b][1]){ return -1}
-        if (arr2[a][1]>arr2[b][1]){ return 1}
-        return 0
+    // arr1.sort((a,b)=>{
+    //     if (arr2[a][1]<arr2[b][1]){ return -1}
+    //     if (arr2[a][1]>arr2[b][1]){ return 1}
+    //     return 0
+    // })
+    
+
+    //  arr1=arr1.filter((a)=>{
+    //     if(objy[a]!=undefined){
+    //         return true}
+    //     else {
+    //                       rem.push(a)
+    //                       return false;
+    //                   }}).sort((a,b)=>{
+    //     if (objy[a]<objy[b]){ return -1}
+    //     if (objy[a]>objy[b]){ return 1}
+    //     return 0
+    // })                
+
+    return arr1
+    .filter((a)=>{ if(objy[a]!=undefined){return true}else{rem.add(a);
+                  return false;}})
+    .sort((a,b)=>{if(objy[a]<objy[b])return -1;
+       return (objy[a]>objy[b])? 1:0
     })
-    return arr1.concat(rem.sort((a,b)=>a-b))
+    .concat([...rem])
 
 };
+
+
+var relativeSortArray=( arr1,arr2)=>{
+    let rem=new Set()
+    let objy={}
+ arr2.forEach((d,i)=>objy[d]=i)
+
+return arr1
+.filter((a)=>{ if(objy[a]!=undefined){return true}else{rem.add(a);rem=new Set(rem);             return false;}})
+.sort((a,b)=>{if(objy[a]<objy[b])return -1;
+   return (objy[a]>objy[b])? 1:0
+})
+.concat([...rem])
+}
 console.log( relativeSortArray(
-    [2,3,1,3,2,4,6,7,9,2,19], [2,1,4,3,9,6]
+   
+[28,6,22,8,44,17],
+[22,28,8,6]
 ))
+
+
+console.log( new Set([1,2,3,4,67,8]))
