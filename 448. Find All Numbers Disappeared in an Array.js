179682b1353e77e8b1edaa7cc1e91656cdc60b4f
GeorgeChryso <lsgechrys@gmail.com>
2019-08-26 20:23:39 +0300

    448 optimized

--- 448. Find All Numbers Disappeared in an Array.js
+++ 448. Find All Numbers Disappeared in an Array.js
@@ -13,7 +13,7 @@
 // [5,6]
 
 var findDisappearedNumbers = function(A) {
-
+    let answ=[]
     for (let i = 1; i < A.length+1; i++) {
                 
             if (A[Math.abs(A[i-1])-1]>=0){
@@ -21,15 +21,13 @@ var findDisappearedNumbers = function(A) {
             }
          
     }
-    for (let i = 0,j=1; i < A.length; j++,i++) {
-           if(A[i]<0){
-               A.splice(i,1);
-               i-- }
-           else A[i]=j
+    for (let i = 0; i < A.length; i++) {
+           if(A[i]>0){
+              answ.push(i+1 )}
         
      
     }
-    return A
+    return answ
 };
 
 
@@ -47,19 +45,36 @@ var findDisappearedNumbers = function(A) {
     return [...S]
 };
 
-// var findDisappearedNumbers = function(A) {
-//     let l=A.length
-//     A=new Object(A)
-//     let ans=[]
-//     for (let i = 1; i < l+1; i++) {
-                
-//             if (!A.has(i)){
-//                 answ.push(i)
-//             }
-         
-//     }
+ var findDisappearedNumbers = function(nums) {
+  let index = 0;
+ let tmp = null
+while (index < nums.length) {
+ let n1 = nums[index]
+      if (index + 1 !== n1) {
+        let n2 = nums[n1 - 1]
+                if (n1 !== n2) {
+                    tmp = nums[index]
+                    nums[index] = nums[n1 - 1]
+                    nums[n1 - 1] = tmp
+                }
+                else {
+                    index++
+                }
+            }
+        else {
+        index++
+    }
+}
 
-//     return answ
+index = 0;
+for (let i = 0; i < nums.length; i++) {
+  if (i + 1 != nums[i]) {
+    nums[index++] = i + 1
+  }
+}
+nums.length = index
+return nums
+};
 // };
 
 console.log(findDisappearedNumbers(
@@ -68,3 +83,5 @@ console.log(findDisappearedNumbers(
   // [1,2,3,4,4]
  // [2,3,1,5,1]
 ))
+
+console.log()
