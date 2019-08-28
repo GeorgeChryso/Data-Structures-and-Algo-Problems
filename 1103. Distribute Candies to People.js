d9b7ebedcfb1cd460bfec9fafe0d5e0056c2f3de
GeorgeChryso <lsgechrys@gmail.com>
2019-08-28 21:49:03 +0300

    1052 opti

--- 1103. Distribute Candies to People.js
+++ 1103. Distribute Candies to People.js
@@ -12,23 +12,16 @@
 // SIMPLE SOLUTION, WEAK
 var distributeCandies = function(C, N) {
     let answ =Array(N).fill(0)
+    answ[0]=1
     for (var  i=1;  C>0 ;i++) {
-        var cur = Math.min(C, i);
-        answ[(i-1)%N]+=cur;
-        C-=cur       
+        answ[(i-1)%N]+=Math.min(C, i);
+        C-=Math.min(C, i);    
     } 
-    
-    
-
-
     return answ
 };
 
-console.log(
-    distributeCandies(
-  60,4
-    )
-)
+
+
 
 //DYNAMIC PROGRAMMING SOLUTION, WEAK
 var distributeCandies = function(C, N) {
@@ -47,3 +40,61 @@ var distributeCandies = function(C, N) {
     dfs(C, 1)
     return result;
 };
+
+// Arithmetic progression usage
+var distributeCandies = function(C, N) {
+    let answ =Array(N).fill(0)
+    function SumAt(n){
+        return n*(n+1)/2
+
+    }
+    let D=C+1-1
+    for (var  i=0;  0<D ;i++) {
+
+        for (var j =0 ;  SumAt(i+j*N)<=C ; j++) {
+            
+            answ[(i)%N]+= SumAt(i+j*N+1)<C? 
+            (1+i+j*N) 
+            :
+             ( C- SumAt(i+j*N))
+        }
+        D-=answ[i]
+    } 
+
+    return answ
+};
+// MORE MATHS
+var distributeCandies = function(C, N) {
+    let answ =Array(N).fill(0)
+    function charAtPos(a1,n,v){
+        return a1+(n-1)*v
+    }
+    function SumAt(a1,n,v){
+        return n*(a1+charAtPos(a1,n,v))/2
+
+    }
+    let D=C+1-1
+    for (var  i=0;  0<D ;i++) {
+
+       var j=0
+       while(SumAt(1,i+j*N,1)<=C){j++}
+        j--;
+        answ[i%N]=SumAt(1,1+i+j*N,1)<C? 
+        SumAt(i%N,i+j*N+1,N)
+        :
+         (  SumAt(i%N,i+j*N ,N)+C-charAtPos(1,i+j*N+1,1) );
+        D-=answ[i]
+    } 
+    answ[0]++
+    console.log('broke at D='+D)
+
+    return answ
+};
+
+console.log(
+    distributeCandies(
+ 10,3
+    )
+)
+
+// 2 2 3
