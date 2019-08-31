94b2825ed92a00b372207a93071f61934484aa68
GeorgeChryso <lsgechrys@gmail.com>
2019-08-31 23:52:57 +0300

    730 almost

---
+++ 406. Queue Reconstruction by Height.js
@@ -0,0 +1,7 @@
+// Suppose you have a random list of people standing in a queue. Each person is described by a pair of integers (h, k), where h is the height of the person and k is the number of people in front of this person who have a height greater than or equal to h. Write an algorithm to reconstruct the queue.
+
+
+var reconstructQueue = function(people) {
+
+ 
+};
---
+++ 739. Daily Temperatures.js
@@ -0,0 +1,26 @@
+var dailyTemperatures = function(T) {
+    let answ=[]
+    for (let i = 0; i < T.length; i++) {
+        
+        if (T[i]==100||i==T.length-1){
+            answ[i]=0
+            continue
+        }
+        for (var j = i+1; T[i]>=T[j]&&j<(T.length-1); j++){
+            
+           
+        }
+        if( j==T.length-1){
+            answ[i]=0
+        }else 
+        {answ[i]=j-i}
+
+        console.log(answ+'')
+    }
+
+return answ
+};
+
+console.log(dailyTemperatures(
+    [55,38,53,81,61,93,97,32,43,78]
+    ))
