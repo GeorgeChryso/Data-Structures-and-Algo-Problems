a37be2f0d441807e1d222d224ad0c0df7b47263a
GeorgeChryso <lsgechrys@gmail.com>
2019-08-31 15:13:20 +0300

    new

--- 1137. N-th Tribonacci Number.js
+++ 1137. N-th Tribonacci Number.js
@@ -7,9 +7,9 @@
 
 var tribonacci = function(n) {
     let arr=[0,1,1]    
-    for (let i =3; i <= n; i++) {
-arr[i]=arr[i-3]+arr[i-2]+arr[i-1]        
-    }
+        for (let i =3; i <= n; i++) {
+             arr[i]=arr[i-3]+arr[i-2]+arr[i-1]        
+        }
     return arr[n]
 };
 console.log(tribonacci(36))
