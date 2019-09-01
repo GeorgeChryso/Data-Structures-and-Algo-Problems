15612f37a36b3602ec023f9f17ce00a6f1187ae7
GeorgeChryso <lsgechrys@gmail.com>
2019-09-01 23:38:21 +0300

    739 optimized

--- 739. Daily Temperatures.js
+++ 739. Daily Temperatures.js
@@ -1,26 +1,58 @@
 var dailyTemperatures = function(T) {
-    let answ=[]
-    for (let i = 0; i < T.length; i++) {
+//     for (let i = 0; i < T.length; i++) {
         
-        if (T[i]==100||i==T.length-1){
-            answ[i]=0
-            continue
-        }
-        for (var j = i+1; T[i]>=T[j] && ( j<=(T.length -1)); j++){
+//         if (T[i]==100||i==T.length-1){
+//             T[i]=0
+//             continue
+//         }
+//         for (var j = i+1; T[i]>=T[j] && ( j<=(T.length -1)); j++){
             
            
-        }
-        if( j>T.length-1){
-            answ[i]=0
-        }else 
-        {answ[i]=j-i
-        }
+//         }
+//         if( j>T.length-1){
+//             T[i]=0
+//         }else 
+//         {T[i]=j-i
+//         }
+
+//     }
+
+
+
+// return T
+
+
+return T.map((d,i)=>{
+    for (var j = i+1; d>=T[j] && ( j<=(T.length -1)); j++){
 
     }
+    if( j>T.length-1){
+        return 0
+    }else 
+    { return j-i
+    }
+})
 
-return answ
 };
 
+var dailyTemperatures = function(T) {
+    let res = Array.from({length:T.length},x=>0);
+    let stack = [];
+    for(let i=0; i<T.length; i++){
+        console.log('i='+i,stack)
+        let stackLast=stack[stack.length-1]
+      console.log(stackLast,T[stackLast],T[i] ,T[stackLast]<T[i],'\n')
+        while(stack.length>0 &&
+             T[stackLast]<T[i]){
+    		let j = stack.pop();
+    		res[j] = i-j;
+    	}
+    	stack.push(i);
+    }
+    return res;
+}; 
 console.log(dailyTemperatures(
     [55,38,53,81,61,93,97,32,43,78]
     ))
+
+
