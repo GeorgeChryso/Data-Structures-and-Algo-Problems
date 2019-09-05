aa99230077084fa964423b32ea6c3cdb0eb300a7
GeorgeChryso <lsgechrys@gmail.com>
2019-09-05 23:25:30 +0300

    976 opti

--- 976. Largest Perimeter Triangle.js
+++ 976. Largest Perimeter Triangle.js
@@ -12,3 +12,12 @@ var largestPerimeter = function(A) {
 	 }
 	return 0;  
 }
+
+var largestPerimeter = function(A) {
+	A.sort(function(a, b) { return b - a; });    
+	for(let i=0;i<A.length-2;i++){
+		let sum = A[i+1] + A[i+2];
+		if (sum > A[i]) return sum + A[i];
+	 }
+	return 0;  
+}
