cdc5c8987c4abbfe2809972eef71b5b0835deaf0
GeorgeChryso <lsgechrys@gmail.com>
2019-09-08 02:08:05 +0300

    1013 opti

--- 1013. Partition Array Into Three Parts With Equal Sum.js
+++ 1013. Partition Array Into Three Parts With Equal Sum.js
@@ -1,15 +1,18 @@
 // //Given an array A of integers, return true if and only if we can partition the array into three non-empty parts with equal sums.
 
 // Formally, we can partition the array if we can find indexes i+1 < j with (A[0] + A[1] + ... + A[i] == A[i+1] + A[i+2] + ... + A[j-1] == A[j] + A[j-1] + ... + A[A.length - 1])
-
 var canThreePartsEqualSum = function(A) {
     var sum=A.reduce((a,b)=>a+b)
+    if( sum%3 )return false
     var sums=[0,0]
    
     for (let i = 0; i < A.length-1; i++) {
         sums[0]+=A[i]
+        if(sums[0]!=Math.floor(sum/3)){continue;}
+        console.log(i)
         for (let j = i+1; j < A.length-1; j++) {
             sums[1]+=A[j]
+            if(sums[1]!=Math.floor(sum/3)){continue;}
 
             if(sums[0]==sums[1]&& (sum-sums[0]-sums[1])==sums[1]){ return true}
 
@@ -18,6 +21,9 @@ var canThreePartsEqualSum = function(A) {
         sums[1]=0        
     }
     return false
+
+
+
 };
 
 console.log(canThreePartsEqualSum(
