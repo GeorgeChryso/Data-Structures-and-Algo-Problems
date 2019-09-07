cc02e3201aaf495ce010941de81b6a537f87db0a
GeorgeChryso <lsgechrys@gmail.com>
2019-09-08 01:47:22 +0300

    1013 solved

--- 1013. Partition Array Into Three Parts With Equal Sum.js
+++ 1013. Partition Array Into Three Parts With Equal Sum.js
@@ -4,28 +4,21 @@
 
 var canThreePartsEqualSum = function(A) {
     var sum=A.reduce((a,b)=>a+b)
-    console.log(sum)
-    function sumAt(i,j){
-        console.log(A.slice(i,j),A.slice(i,j).reduce((a,b)=>a+b))
-     return A.slice(i,j).reduce((a,b)=>a+b)
+    var sums=[0,0]
+   
+    for (let i = 0; i < A.length-1; i++) {
+        sums[0]+=A[i]
+        for (let j = i+1; j < A.length-1; j++) {
+            sums[1]+=A[j]
 
-    }
-    for (let i = 1; i < A.length-2; i++) {
-        console.log('DATS i')
-        let z=sumAt(0,i)
-        for (let j = i; j < A.length-1; j++) {
-            let q=sumAt(i,j+1)
-            if((sum-z-q)==z && z==q){
-                console.log(
-                    i,j,'\n',
-                    A.slice(0,i), A.slice(i,j+1) , A.slice(j+1,A.length)
-                )
-                return true
-            }
-        }        
+            if(sums[0]==sums[1]&& (sum-sums[0]-sums[1])==sums[1]){ return true}
+
+           
+        }
+        sums[1]=0        
     }
     return false
 };
 
 console.log(canThreePartsEqualSum(
-    [0,2,1,-6,6,-7,9,1,2,0,1]    ))
+    [18,12,-18,18,-19,-1,10,10] ))
