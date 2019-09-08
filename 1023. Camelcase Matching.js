cbb2c127cd39be2291405d265f9a60545fa29005
GeorgeChryso <lsgechrys@gmail.com>
2019-09-09 00:58:33 +0300

    1023 solved

--- 1023. Camelcase Matching.js
+++ 1023. Camelcase Matching.js
@@ -4,23 +4,21 @@
 
 var camelMatch = function(queries, pattern) {
     let arr=[]
-    pattern=pattern.split('')
-    for (let i=0,j=-1;i<pattern.length;i++) {
-        if (/[A-Z]/.test(pattern[i])) {
-            j++          
-            arr[j]=['']
-        }
-        arr[j]+=pattern[i]
-    }
-    var regy=new RegExp(arr.join('([a-z])*')+'([a-z])*')
+    arr=pattern.split('')
+  
+    var regy=new RegExp('^([a-z])*'+arr.join('([a-z])*')+'([a-z])*$')
+    console.log(arr,regy)
     return queries.map(
         (d)=>(regy.test(d)?true:false)
     )
 };
 console.log(camelMatch(
-   ["FooBar","FooBarTest","FootBall","FrameBuffer","ForceFeedBack"],"FoBaT"
+    ["FooBar","FooBarTest","FootBall","FrameBuffer","ForceFeedBack"],
+    "FB"
 ))
 
-var rego=/Fo([a-z])*Ba([a-z])*/.test('FooBar')
+console.log(camelMatch(
+    ["aksvbjLiknuTzqon","ksvjLimflkpnTzqn","mmkasvjLiknTxzqn","ksvjLiurknTzzqbn","ksvsjLctikgnTzqn","knzsvzjLiknTszqn"],
+"ksvjLiknTzqn"
 
-console.log(rego)
+))
