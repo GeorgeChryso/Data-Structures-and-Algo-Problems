eb43317575e5162df45bc86e306b49041d5cfa2c
GeorgeChryso <lsgechrys@gmail.com>
2019-09-12 21:31:57 +0300

    485

--- 485. Max Consecutive Ones.js
+++ 485. Max Consecutive Ones.js
@@ -3,14 +3,16 @@ var findMaxConsecutiveOnes = function(nums) {
     let cur=0
     let max=0
     
-    nums.forEach((d)=>{
-        if(d)cur++
-        else{
-            max=Math.max(max,cur)
-            cur=0
-        }
-        
-    })
+ for (let i = 0; i < nums.length; i++) {
+    if(nums[i]){
+    cur++
+    continue}
+    
+    max=Math.max(max,cur)
+    cur=0
+  
+ }
+
     return max=Math.max(max,cur)
 
 };
