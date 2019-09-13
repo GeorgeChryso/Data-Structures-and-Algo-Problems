61ba9ad240e191adc170e62d3243932475f5924e
GeorgeChryso <lsgechrys@gmail.com>
2019-09-13 21:01:05 +0300

    788;

---
+++ 788. Rotated Digits.js
@@ -0,0 +1,22 @@
+
+var rotatedDigits = function(N) {
+var goods={
+    0:0,
+    1:1,
+    8:8,
+    2:5,
+    5:2,
+    6:9,
+    9:6
+}
+
+var count=0
+
+for (let i = 0; i < N.length; i++) {
+    const element = array[i];
+    
+}
+
+return count
+    
+};
