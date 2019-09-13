4e061b156bface901c4df6af97342f9b545dcd84
GeorgeChryso <lsgechrys@gmail.com>
2019-09-13 21:32:16 +0300

    788 solved

--- 788. Rotated Digits.js
+++ 788. Rotated Digits.js
@@ -11,27 +11,31 @@ var goods={
 }
 
 var count=0
-function amIvalid(d){
-    d=String(d).split('')
-    for (let i = 0; i < d.length; i++) {
-        if(goods[d[i]]==undefined){
-        return false     }
-        else{
-            d[i]=goods[d[i]]
-        }
-    }
-    return Number(d)
-}
-for (let i = 1; i < N; i++) {
-   if (amIvalid(i) && amIvalid(i)!=i){
-       console.log(i,amIvalid(i))
-       count++
-   }
+
+for (let i = 1; i <=N; i++) {
+ 
     
+   d=String(i).split('')
+   for (let j = 0; j < d.length; j++) {
+    if(goods[d[j]]==undefined){
+        d=i
+        break;
+    }
+    else{
+        d[j]=goods[d[j]]
+    }
+    }
+    if( d[0]!=undefined){ d=Number(d.join(''))}
+    
+if (d!=i){
+    count++
+}
+
+
 }
 
 return count
     
 };
 
-console.log(rotatedDigits(10))
+console.log(rotatedDigits(857))
