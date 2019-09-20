e1c6153d52320f17c3c43cbde5326f56efe27291
GeorgeChryso <lsgechrys@gmail.com>
2019-09-20 04:11:53 +0300

    287 solved

--- 287. Find the Duplicate Number.js
+++ 287. Find the Duplicate Number.js
@@ -1,9 +1,16 @@
 
 // You must not modify the array (assume the array is read only).
 // You must use only constant, O(1) extra space.
+
 // Your runtime complexity should be less than O(n2).
 // There is only one duplicate number in the array, but it could be repeated more than once..
 
+var findDuplicate = function(nums) {
+    nums= nums.sort()
+   for (let i = 0; i < nums.length; i++) {
+    if( nums[i]==nums[i+1])return nums[i]}
+  }
+// terrible btw
 
 var findDuplicate = function(nums) {
     var z={}
@@ -14,6 +21,9 @@ var findDuplicate = function(nums) {
 
     console.log(z)
 };
+// fast but beats the cause
+
+
 
 console.log(findDuplicate(
     [1,3,4,2,2]
