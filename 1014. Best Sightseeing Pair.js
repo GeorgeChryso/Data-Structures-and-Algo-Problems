0974236d6d5d8a021e3c70066698e97684f4fdcf
GeorgeChryso <lsgechrys@gmail.com>
2019-09-22 22:15:14 +0300

    1014

---
+++ 1014. Best Sightseeing Pair.js
@@ -0,0 +1,25 @@
+// Given an array A of positive integers, A[i] represents the value of the i-th sightseeing spot, and two sightseeing spots i and j have distance j - i between them.
+
+// The score of a pair (i < j) of sightseeing spots is (A[i] + A[j] + i - j) : the sum of the values of the sightseeing spots, minus the distance between them.
+
+// Return the maximum score of a pair of sightseeing spots.
+
+ 
+
+// Example 1:
+
+// Input: [8,1,5,2,6]
+// Output: 11
+// Explanation: i = 0, j = 2, A[i] + A[j] + i - j = 8 + 5 + 0 - 2 = 11
+
+var maxScoreSightseeingPair = function(A) {
+    let max=0
+    for (let i = 0; i < A.length; i++) {
+        for (let j = 0; j < A.length; j++) {
+            max=Math.max(max,A[i]+A[j]+i-j)            
+        }
+        
+    }
+    return max
+};
+
