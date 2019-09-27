b6804c3aee6d1bc912410aaaa404c0523d58abf9
GeorgeChryso <lsgechrys@gmail.com>
2019-09-27 22:04:34 +0300

    msg

--- 477. Total Hamming Distance.js
+++ 477. Total Hamming Distance.js
@@ -27,7 +27,7 @@ var totalHammingDistance = function(nums) {
         }
     }
     return total
-};//fonyf
+};//fonyfycuhg
 
 var totalHammingDistance = function(nums) {
     return Array.from(Array(32), (_,i)=>1<<i)
