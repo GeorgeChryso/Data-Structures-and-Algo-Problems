c92c49bde7e0c65edfd869f2fc304e71ba6ba55c
GeorgeChryso <lsgechrys@gmail.com>
2019-10-04 20:48:52 +0300

    optimizing

--- 539. Minimum Time Difference.js
+++ 539. Minimum Time Difference.js
@@ -51,16 +51,17 @@ var findMinDifference = function(A) {
 
 
 var findMinDifference=(A)=>{
-    A=A.map(d=>[Number(d[0]+d[1]),Number(d[3]+d[4])]).sort((a,b)=>(a[0]-b[0]==0?(a[1]-b[1]):(a[0]-b[0])))
+    A=A.map(d=>[Number(d[0]+d[1]),Number(d[3]+d[4])])
+    .sort((a,b)=>(a[0]==b[0]?(a[1]-b[1]):(a[0]-b[0]) ))
 
-    var calc=(ta,tb)=>{
+    var minuteDifference=(ta,tb)=>{
         return (tb[0]-ta[0])*60+tb[1]-ta[1]
 
     }
     console.log(A)
     var result=Infinity
     for (let i = 0; i < A.length-1; i++) {
-        result=Math.min(result,calc(A[i],A[i+1]))
+        result=Math.min(result,minuteDifference(A[i],A[i+1]))
         console.log(result)        
     }
 
@@ -69,9 +70,31 @@ var findMinDifference=(A)=>{
    return Math.min(result,A[0][0]*60+A[0][1]+(24-A[A.length-1][0])*60-A[A.length-1][1]
    )
 
-   A[0][0]*60+A[0][1]+(24-A[A.length-1][0])*60-A[A.length-1][1]
 
 }
+
+var findMinDifference=(A)=>{
+    var dict=Array(1440).fill(false)
+    var result=Infinity
+    for (let i = 0; i < A.length; i++) {
+        var []=A[i]// "23:49"
+        dict[
+            (A[i][0]+A[i][1])*60
+            +(A[i][3]+A[i][4])*1
+            ]=true       
+    }
+    var temp=Infinity
+    for(let c of dict){
+        if(c==true){
+            result=Math.min(result,temp)
+            temp=0
+        }
+        temp++
+    }
+
+    return result
+}
+
 console.log(findMinDifference(
     ["12:01","00:10","00:01"]
 
