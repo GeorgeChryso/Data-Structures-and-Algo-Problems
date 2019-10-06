59b97e77b325c61fdc867c91535b61886e4b2d14
GeorgeChryso <lsgechrys@gmail.com>
2019-10-06 13:14:26 +0300

    1109 to be posted

--- 1109. Corporate Flight Bookings.js
+++ 1109. Corporate Flight Bookings.js
@@ -28,15 +28,29 @@ i1<i2?
 
 */
 var corpFlightBookings = function(bookings, n) {
+
+    // create the result Array which we are going to 
+    // utilize for our end result. Fill each (flight) with 0 seats
     var result=Array(n).fill(0)
   
+    // traverse the bookings array, destructuring its contents with ES6 for easier processing
     for(let [i,j,k] of bookings){
 
-            result[i-1]+=k
-            if(j<n)result[j]-=k
-        console.log(result)
+        // !Basically say that: 
+        // From the index(flight) i-1 and onwards, I want to
+        // add k-seats to each.
+        // Because 1st flight points to index 0 of the result Array we re using result[i-1] and not result[i]
+        result[i-1]+=k
+
+        // !Basically say that:
+        // After the index j, I dont want the k-seats anymore
+        if(j<n) result[j]-=k
+        // if j was equal to n that would mean that I dont really care what happens after I added k inbetween i and j-1. j-1 is the last element I really care about
     }
+
+    // traverse the modified result array
     for (let i = 1; i < result.length; i++) {
+        // Accumulate the final result (check out the provided image for this step)
         result[i]+=result[i-1] 
 
     }
