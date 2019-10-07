77d04355278d431e1fc8553b91a7373046110ec8
GeorgeChryso <lsgechrys@gmail.com>
2019-10-07 20:14:52 +0300

    386 needs opti

--- 386. Lexicographical Numbers.js
+++ 386. Lexicographical Numbers.js
@@ -12,3 +12,40 @@ var lexicalOrder = function(n) {
   }  
   return result.sort()
 };
+
+console.log(lexicalOrder(
+    123
+
+))
+
+
+var lexicalOrder=(n)=>{
+    var result=[]
+    var objy={
+        1:[],
+        2:[],
+        3:[],
+        4:[],
+        5:[],
+        6:[],
+        7:[],
+        8:[],
+        9:[]
+    }
+for (let i = 0; i < n.length; i++) {
+
+    for (let j = 1; j <= n; j**10) {
+        result.push(j)
+        for (let z = 1; z < j; z++) {
+            objy[String(j[0])].push(z)
+        }
+
+    }
+    
+}
+
+
+}
+
+
+//mexri 5 000 000
