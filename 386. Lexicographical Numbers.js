99139ec2bd134fee3c7408284f5c9459df3e2fc9
GeorgeChryso <lsgechrys@gmail.com>
2019-10-08 12:56:10 +0300

    628 Max Stack Size Exc

--- 386. Lexicographical Numbers.js
+++ 386. Lexicographical Numbers.js
@@ -38,28 +38,31 @@ var lexicalOrder = function(n) {
 
 
 var lexicalOrder = function(n) {
-    var result = [];
 
-    function getNumberByOrder( start, end) {
-        
-        while(start <= end ){
-         console.log(result)
-         if(start>n)break
-         result.push(start);   
-         getNumberByOrder(start*10, start*10+9);
-         start++   
-         
+        function pushFromTo( start, end ) {
+            /*essentially this function pushes every integer between start and end to the results array and breaks whenever 
+            either the end or n is reached */
+            while( start<=end && start<=n ){ 
+                result.push(start);   
+
+                // The nesting occurs, new iterations are attempted within this step utilizing the observed pattern
+                pushFromTo(start*10, start*10+9)
+
+                start++   
+            }
+
         }
-   }
 
-   
-   
-   getNumberByOrder( 1, 9);
+   var result = [];
+
+   // start the recursion 
+   pushFromTo( 1, 9);
+
    return result;
  };
  
 
  console.log(lexicalOrder(
-    123
+  112
 
 ))
