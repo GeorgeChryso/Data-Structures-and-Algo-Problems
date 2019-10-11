f6b8acd78f66add8d3db1c2919fa1106e23ec97c
GeorgeChryso <lsgechrys@gmail.com>
2019-10-11 21:11:12 +0300

    844 O(n) O(1) improved space compl

--- 844. Backspace String Compare.js
+++ 844. Backspace String Compare.js
@@ -1,30 +1,47 @@
 // Given two strings S and T, return if they are equal when both are typed into empty text editors. # means a backspace character.
 
 var backspaceCompare = function(S, T) {
-   var OS=[],OT=[]
-   
-   for (let i = 0; i < Math.max(S.length,T.length); i++) {
-        if( S[i]!==undefined){
-            if(S[i]==='#')OS.pop()
-            else OS.push(S[i])
+   var curr=0,endofS=0,endofT=0
+   S=Array.from(S)
+   T=Array.from(T)
+   for (let i = 0; i < S.length; i++) {
+        if(S[i]=='#'){
+            curr?curr--:null
+            console.log( curr)
+
+
         }
-        if( T[i]!==undefined){
-            if(T[i]==='#')OT.pop()
-            else OT.push(T[i])
-        } 
-         
+        else{
+            S[curr]=S[i]
+            curr++
+            console.log(S+'',curr)
+
+        }
    }
-   console.log(OS,OT)
-   if(OS.length!==OT.length)return false   
-   
-   for (let i = 0; i < OS.length; i++) {
-     if(OS[i]!==OT[i])return false       
+   endofS=curr
+   for (var i = 0,curr=0; i < T.length; i++) {
+        if(T[i]=='#'){
+            curr?curr--:null
+            console.log( curr)
+        }
+        else{
+            T[curr]=T[i]
+           
+            curr++
+            console.log(T+'',curr)
+        }
+    }
+    endofT=curr
+    if(endofT!=endofS)return false
+    
+    for (let i = 0; i < endofT; i++) {
+     if(S[i]!==T[i])return false       
    }
    return true
 };
 
 
 console.log(backspaceCompare(
-    "ab#c",
-"ad#c"
+    "gtc#uz#",
+    "gtc#uz##"
 ))
