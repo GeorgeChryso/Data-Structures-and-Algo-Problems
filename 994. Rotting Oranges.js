4672da8446d38f5cc946ba3e6312c711a090fcac
GeorgeChryso <lsgechrys@gmail.com>
2019-10-13 21:26:26 +0300

    994 opt

--- 994. Rotting Oranges.js
+++ 994. Rotting Oranges.js
@@ -58,43 +58,6 @@ var orangesRotting = function(A) {
   return inflag?-1:result
 };
 
-var orangesRotting = function(grid) {
-    let row = grid.length;
-    let col = grid[0].length;
-    let fresh = 0;
-    let queue = [];
-    let minutes = 0;
-    for (let i = 0; i < row; i++) {
-      for (let j = 0; j < col; j++) {
-        if (grid[i][j] === 1) {
-          fresh += 1;
-        } else if (grid[i][j] === 2) {
-          queue.push([i, j]);
-        }
-      }
-    }
-    let movement = [[1, 0], [-1, 0], [0, 1], [0, -1]];
-    while (queue.length !== 0 && fresh) {
-      let size = queue.length;
-      while (size--) {
-        let curCell = queue.shift();
-        const x = curCell[0];
-        const y = curCell[1];
-        for (let d = 0; d < 4; d++) {
-          const tx = x + movement[d][0];
-          const ty = y + movement[d][1];
-          if (tx < 0 || tx >= row || ty < 0 || ty >= col || grid[tx][ty] !== 1) {
-            continue;
-          }
-          fresh--;
-          grid[tx][ty] = 2;
-          queue.push([tx, ty]);
-        }
-      }
-      minutes++;
-    }
-    return fresh ? -1 : minutes;
-  };
 
   var orangesRotting = function(grid) {
     let queue = [];
@@ -200,6 +163,7 @@ var orangesRotting = function(grid) {
         dep = Math.max(dep, _dep);
     }
        
+    //scans the matrix for 1, if found returns -1, as its never gonna change
     for(let row = 0; row < grid.length; row++) {
         for(let col = 0; col < grid[row].length; col++) {
             if(grid[row][col] === 1) {
