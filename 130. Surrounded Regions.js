ca67482f1f47962b7f04e7538d7c7200cdf1f717
GeorgeChryso <lsgechrys@gmail.com>
2019-10-15 18:12:52 +0300

    refined solutions & comments

--- 130. Surrounded Regions.js
+++ 130. Surrounded Regions.js
@@ -5,10 +5,12 @@
 
 // Switch all the border to 3s, it is a dfs
 var solve = function(A) {
-    if (A.length<=0)return A
+    if (A.length<=1)return A
+
+    //This function plagues the element itself
+    // and spreads the plague to adjacent ones
     var spread=(i,j)=>{
         if(i<0||j<0||i>A.length-1||j>A[0].length-1)return
-        console.log(i,j)
         if(A[i][j]!='O')return;
         A[i][j]=3
         if(i<A.length-1&& A[i+1][j]=='O'){
@@ -26,36 +28,24 @@ var solve = function(A) {
         return 
     }
     
-    for (let j = 0,i=0; j < A[0].length; j++) {
-        if ( A[i][j]=='O'){
-            spread(i,j)
+
+
+    //plague the elements of first and last row    
+    for (let i = 0; i < A.length; i+=A.length-1) {
+        for (let j = 0; j < A[0].length; j++) {
+            if(A[i][j]=='O')spread(i,j)
+        }        
+    }
+
+    //plague the elements of first and last column    
+    for (let j = 0; j< A[0].length; j+=A[0].length-1) {
+        for (let i = 1; i < A.length-1; i++) {
+            if(A[i][j]=='O')spread(i,j)
         }        
     }
 
 
-    for (let j = 0,i=A.length-1; j < A[0].length; j++) {
-        if ( A[i][j]=='O'){
-            spread(i,j)
-        }        
-    }
-
-    console.log(A.map(d=>d+''))
-
-    for (let i = 0,j=0; i < A.length; i++) {
-        if ( A[i][j]=='O'){
-            spread(i,j)
-        }        
-    }
-    console.log(A.map(d=>d+''))
-
-    for (let i = 0; i < A.length; i++) {
-        if ( A[i][A[0].length-1]=='O'){
-            console.log('call',i)
-            spread(i,A[0].length-1)
-        }        
-    }
-    console.log(A.map(d=>d+''))
-
+     // replace all plagued with 'O' and the rest with 'X'
     for (let i = 0; i < A.length; i++) {
         for (let j = 0; j < A[0].length; j++) {
                 
@@ -65,7 +55,6 @@ var solve = function(A) {
         }
         
     }
-    console.log(A.map(d=>d+''))
 
     return A
 
@@ -73,12 +62,13 @@ var solve = function(A) {
 
 
 // MORE CLEAR BREADTH FIRST SEARCH
+// plagued: is an element that remains 'O' after the switch
 var solve = function(A) {
     if (A.length<=1)return A
    
-    var queue=[]
+    var queue=[]// Here I will store all the plagued elements
 
-    //check the first and last rows
+    //check the first and last rows for plagued elements
     for (let i = 0; i < A.length; i+=A.length-1) {
         for (let j = 0; j < A[0].length; j++) {
             if(A[i][j]=='O')queue.push([i,j])
@@ -100,9 +90,12 @@ var solve = function(A) {
     while(queue.length){
 
         let [i,j]=queue.shift() // shift an element and process it
+
         if(i<0||j<0||i>A.length-1||j>A[0].length-1||A[i][j]==3)continue; // nvm if its already processed or outside the borders
     
         A[i][j]=3
+
+        //All the directions of an element
         let dir=[[i+1,j], [i,j+1],[i,j-1],[i-1,j]]
 
 
