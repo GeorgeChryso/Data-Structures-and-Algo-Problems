7f4217fb20543b0331bfe438e89ec97ab8832f1c
GeorgeChryso <lsgechrys@gmail.com>
2019-10-15 18:04:13 +0300

    130 Optimized clear BFS

--- 130. Surrounded Regions.js
+++ 130. Surrounded Regions.js
@@ -2,6 +2,8 @@
 
 // A region is captured by flipping all 'O's into 'X's in that surrounded region.
 
+
+// Switch all the border to 3s, it is a dfs
 var solve = function(A) {
     if (A.length<=0)return A
     var spread=(i,j)=>{
@@ -23,7 +25,6 @@ var solve = function(A) {
         }
         return 
     }
-    console.log(A.map(d=>d+''))
     
     for (let j = 0,i=0; j < A[0].length; j++) {
         if ( A[i][j]=='O'){
@@ -31,7 +32,6 @@ var solve = function(A) {
         }        
     }
 
-    console.log(A.map(d=>d+''))
 
     for (let j = 0,i=A.length-1; j < A[0].length; j++) {
         if ( A[i][j]=='O'){
@@ -71,18 +71,71 @@ var solve = function(A) {
 
 };
 
+
+// MORE CLEAR BREADTH FIRST SEARCH
+var solve = function(A) {
+    if (A.length<=1)return A
+   
+    var queue=[]
+
+    //check the first and last rows
+    for (let i = 0; i < A.length; i+=A.length-1) {
+        for (let j = 0; j < A[0].length; j++) {
+            if(A[i][j]=='O')queue.push([i,j])
+        }        
+    }
+
+    //check the first and last columns
+    for (let j = 0; j< A[0].length; j+=A[0].length-1) {
+        for (let i = 1; i < A.length-1; i++) {
+            if(A[i][j]=='O')queue.push([i,j])
+        }        
+    }
+
+
+
+    //queue now has the border's plagued elements
+    // I will keep adding to the queue the 'O's connected 
+    // to the plagued elements and plaguing them aswell
+    while(queue.length){
+
+        let [i,j]=queue.shift() // shift an element and process it
+        if(i<0||j<0||i>A.length-1||j>A[0].length-1||A[i][j]==3)continue; // nvm if its already processed or outside the borders
+    
+        A[i][j]=3
+        let dir=[[i+1,j], [i,j+1],[i,j-1],[i-1,j]]
+
+
+        // process every direction of that element pushing its children into the length
+        for (const d of dir) {
+            if(d[0]<0||d[1]<0||d[0]>A.length-1||d[1]>A[0].length-1)continue;
+
+            if(A[d[0]][d[1]]=='O'){
+                queue.push([d[0],d[1]])
+            }
+        }
+
+
+    }
+
+
+
+    // replace all plagued with 'O' and the rest with 'X'
+    for (let i = 0; i < A.length; i++) {
+        for (let j = 0; j < A[0].length; j++) {
+                
+            if(A[i][j]=='O')A[i][j]='X'
+            else if (A[i][j]==3)A[i][j]='O'
+            else  continue
+        }
+        
+    }
+    return A
+};
+
 console.log(solve(
-    [
-    ["X","O","O","X","X","X","O","X","O","O"],
-    ["X","O","X","X","X","X","X","X","X","X"],
-    ["X","X","X","X","O","X","X","X","X","X"],
-    ["X","O","X","X","X","O","X","X","X","O"],
-    ["O","X","X","X","O","X","O","X","O","X"],
-    ["X","X","O","X","X","O","O","X","X","X"],
-    ["O","X","X","O","O","X","O","X","X","O"],
-    ["O","X","X","X","X","X","O","X","X","X"],
-    ["X","O","O","X","X","O","X","X","O","O"],
-    ["X","X","X","O","O","X","O","X","X","O"]
-]
-))
+    
+    [["O","O"],["O","O"]]
+    
+    ));
 
