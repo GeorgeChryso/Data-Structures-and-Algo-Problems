4ef806dff57ca1488aaadc01939a35a0b44030fa
GeorgeChryso <lsgechrys@gmail.com>
2019-10-16 14:23:28 +0300

    126 commenting & 127

---
+++ 126. Word Ladder II.js
@@ -0,0 +1,4 @@
+// Given two words (beginWord and endWord), and a dictionary's word list, find all shortest transformation sequence(s) from beginWord to endWord, such that:
+
+// Only one letter can be changed at a time
+// Each transformed word must exist in the word list. Note that beginWord is not a transformed word.
--- 127. Word Ladder.js
+++ 127. Word Ladder.js
@@ -1,3 +1,14 @@
+// Given two words (beginWord and endWord), and a dictionary's word list, find the length of shortest transformation sequence from beginWord to endWord, such that:
+
+// Only one letter can be changed at a time.
+// Each transformed word must exist in the word list. Note that beginWord is not a transformed word.
+// Note:
+
+// Return 0 if there is no such transformation sequence.
+// All words have the same length.
+// All words contain only lowercase alphabetic characters.
+// You may assume no duplicates in the word list.
+// You may assume beginWord and endWord are non-empty and are not the same.
 
 //Start End Dictionary
 
@@ -5,7 +16,7 @@
 var ladderLength = function(S, E, D) {
     if (!D.includes(E))return 0
 
-
+    
     var time=1
     var queue=[S]
 
