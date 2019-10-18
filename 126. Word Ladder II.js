3a69b830f380c272aa51feaa94b4ee28a70e4739
GeorgeChryso <lsgechrys@gmail.com>
2019-10-18 19:23:09 +0300

    126 solved

--- 126. Word Ladder II.js
+++ 126. Word Ladder II.js
@@ -48,9 +48,6 @@ var ladderLength = function(S, E, D) {
 
         }
 
-//console.log(`my startPath is `,startPath)
-//console.log(` found the children `,curChildren)
-//console.log(`with info `,childInfo)
 
 
         //create the result sequences for this level
@@ -71,7 +68,6 @@ var ladderLength = function(S, E, D) {
 
               
         }
-//console.log('\n sequences created',result)
 
         // finish filter
         if(finish){
@@ -95,11 +91,11 @@ console.log(time,'\n \n \n \n')
 // WORKS, but MEMORY PROBLEM, HEAP EXCEEDED
 var findLadders = function(S, E, D) {
     if (!D.includes(E))return []
-     // array of sequences [arrays]
     var finish=false
 
     const dict = new Set(D);
     var queue=[ [[S],dict]  ]
+    //queue has elements of type [ sequence, dictionary]
     var time=1
 
 
@@ -107,27 +103,24 @@ var findLadders = function(S, E, D) {
 
     while(queue.length && time<D.length+1){
         curPaths=[]
-        console.log(queue)
 
         //find the children of the parents
         for (let [T,DICTI] of queue) {
             
            let tail=T[T.length-1]
-           console.log(queue,tail)
 
             for (let i = 0; i < tail.length; i++) {
 
 
                 for (let j = 0; j < 26; j++) {
-                    let DICT= new Set(DICTI)
 
                     let possibleTransmutation= tail.slice(0,i)
                     +String.fromCharCode(97 + j) // all letters of the alphabet (IN ORDER)
                     +tail.slice(i + 1);
 
 
-                    if (DICT.has(possibleTransmutation)) {
-                        console.log('DICT HAS',possibleTransmutation)
+                    if (DICTI.has(possibleTransmutation)) {
+                        let DICT= new Set(DICTI)
                         if(possibleTransmutation===E){
                             finish=true
                         }
@@ -145,7 +138,40 @@ var findLadders = function(S, E, D) {
 
         }
 
+        while(queue.length){
 
+            let [T,DICTI]=queue.shift()
+            let tail=T[T.length-1]
+
+            for (let i = 0; i < tail.length; i++) {
+
+
+                for (let j = 0; j < 26; j++) {
+
+                    let possibleTransmutation= tail.slice(0,i)
+                    +String.fromCharCode(97 + j) // all letters of the alphabet (IN ORDER)
+                    +tail.slice(i + 1);
+
+
+                    if (DICTI.has(possibleTransmutation)) {
+                        let DICT= new Set(DICTI)
+                        if(possibleTransmutation===E){
+                            finish=true
+                        }
+                        
+                        
+                        DICT.delete(possibleTransmutation)
+                        
+                        curPaths.push(
+                            [T.concat(possibleTransmutation),DICT]
+                        )
+                    }
+
+                }
+            }
+
+
+        }
        
 
         // finish filter
@@ -169,10 +195,54 @@ var findLadders = function(S, E, D) {
 };
 
 
+var findLadders=function(beginWord, endWord, wordList){
+    let results=[]
+    let visited={}
+    let steps=Number.MAX_SAFE_INTEGER
+    let pathq=[[beginWord]]
+    let wordset=new Set(wordList)
+    
+    while(pathq.length>0){
+        
+        
+        let curpath=pathq.shift()
+        let curword=curpath[curpath.length-1]
+        
+        
+        if(curpath.length>=steps){
+                break
+        }
+        
+        for(let i = 97; i < 123; i++){
+            for(let j=0;j<curword.length;j++){
+            let w=curword.substring(0, j)+String.fromCharCode(i)+curword.substring(j+1)
+            if(w!==curword&&wordset.has(w)){
+               if(!visited[w]){
+                    pathq.push([...curpath,w])
+                }
+                if(w===endWord){
+                    if(curpath.length<steps){
+                        steps=curpath.length+1
+                    }
+                    results.push([...curpath,w])
+                }
+                
+            }
+        }
+    }
+        visited[curword]=true
+        
+        
+        
+    }
+    
+    return results
+}
+
 console.log(findLadders(
-    "hit",
-    "cog",
-    ["hot","dot","dog","lot","log","cog"]
+"cet",
+"ism",
+["kid","tag","pup","ail","tun","woo","erg","luz","brr","gay","sip","kay","per","val","mes","ohs","now","boa","cet","pal","bar","die","war","hay","eco","pub","lob","rue","fry","lit","rex","jan","cot","bid","ali","pay","col","gum","ger","row","won","dan","rum","fad","tut","sag","yip","sui","ark","has","zip","fez","own","ump","dis","ads","max","jaw","out","btu","ana","gap","cry","led","abe","box","ore","pig","fie","toy","fat","cal","lie","noh","sew","ono","tam","flu","mgm","ply","awe","pry","tit","tie","yet","too","tax","jim","san","pan","map","ski","ova","wed","non","wac","nut","why","bye","lye","oct","old","fin","feb","chi","sap","owl","log","tod","dot","bow","fob","for","joe","ivy","fan","age","fax","hip","jib","mel","hus","sob","ifs","tab","ara","dab","jag","jar","arm","lot","tom","sax","tex","yum","pei","wen","wry","ire","irk","far","mew","wit","doe","gas","rte","ian","pot","ask","wag","hag","amy","nag","ron","soy","gin","don","tug","fay","vic","boo","nam","ave","buy","sop","but","orb","fen","paw","his","sub","bob","yea","oft","inn","rod","yam","pew","web","hod","hun","gyp","wei","wis","rob","gad","pie","mon","dog","bib","rub","ere","dig","era","cat","fox","bee","mod","day","apr","vie","nev","jam","pam","new","aye","ani","and","ibm","yap","can","pyx","tar","kin","fog","hum","pip","cup","dye","lyx","jog","nun","par","wan","fey","bus","oak","bad","ats","set","qom","vat","eat","pus","rev","axe","ion","six","ila","lao","mom","mas","pro","few","opt","poe","art","ash","oar","cap","lop","may","shy","rid","bat","sum","rim","fee","bmw","sky","maj","hue","thy","ava","rap","den","fla","auk","cox","ibo","hey","saw","vim","sec","ltd","you","its","tat","dew","eva","tog","ram","let","see","zit","maw","nix","ate","gig","rep","owe","ind","hog","eve","sam","zoo","any","dow","cod","bed","vet","ham","sis","hex","via","fir","nod","mao","aug","mum","hoe","bah","hal","keg","hew","zed","tow","gog","ass","dem","who","bet","gos","son","ear","spy","kit","boy","due","sen","oaf","mix","hep","fur","ada","bin","nil","mia","ewe","hit","fix","sad","rib","eye","hop","haw","wax","mid","tad","ken","wad","rye","pap","bog","gut","ito","woe","our","ado","sin","mad","ray","hon","roy","dip","hen","iva","lug","asp","hui","yak","bay","poi","yep","bun","try","lad","elm","nat","wyo","gym","dug","toe","dee","wig","sly","rip","geo","cog","pas","zen","odd","nan","lay","pod","fit","hem","joy","bum","rio","yon","dec","leg","put","sue","dim","pet","yaw","nub","bit","bur","sid","sun","oil","red","doc","moe","caw","eel","dix","cub","end","gem","off","yew","hug","pop","tub","sgt","lid","pun","ton","sol","din","yup","jab","pea","bug","gag","mil","jig","hub","low","did","tin","get","gte","sox","lei","mig","fig","lon","use","ban","flo","nov","jut","bag","mir","sty","lap","two","ins","con","ant","net","tux","ode","stu","mug","cad","nap","gun","fop","tot","sow","sal","sic","ted","wot","del","imp","cob","way","ann","tan","mci","job","wet","ism","err","him","all","pad","hah","hie","aim","ike","jed","ego","mac","baa","min","com","ill","was","cab","ago","ina","big","ilk","gal","tap","duh","ola","ran","lab","top","gob","hot","ora","tia","kip","han","met","hut","she","sac","fed","goo","tee","ell","not","act","gil","rut","ala","ape","rig","cid","god","duo","lin","aid","gel","awl","lag","elf","liz","ref","aha","fib","oho","tho","her","nor","ace","adz","fun","ned","coo","win","tao","coy","van","man","pit","guy","foe","hid","mai","sup","jay","hob","mow","jot","are","pol","arc","lax","aft","alb","len","air","pug","pox","vow","got","meg","zoe","amp","ale","bud","gee","pin","dun","pat","ten","mob"]
     
     
     )
