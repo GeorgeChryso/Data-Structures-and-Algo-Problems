51aa46f027db3545ce7aff20e2fdfb53fc4d1979
GeorgeChryso <lsgechrys@gmail.com>
2019-10-20 22:39:24 +0300

    34 optimized

--- 34. Find First and Last Position of Element in Sorted Array.js
+++ 34. Find First and Last Position of Element in Sorted Array.js
@@ -6,42 +6,74 @@
 
 
 var searchRange = function(A, T) {
+
+
     var finished=false
     var tIndex=-1
     var result=[-1,-1]
+    if(!A.length){return result}
 
-    var dp=(arr)=>{
-        if(!arr.length || finished)return
-        if (arr.length%2){
-            var splitIndex=(arr.length-1)/2
+    var dp=(start,end)=>{
+        let length=1+end-start
+
+        if(!length || finished)return
+
+        if (length%2){
+            var splitIndex=(length-1)/2
         }
         else {
-            var splitIndex=arr.length/2-1
+            var splitIndex=length/2-1
         }
-        
-        if(arr[splitIndex]==T){
+        console.log(`CALLED [${start},${end}] with splitIndex ${splitIndex}`)
+
+        if(A[start+splitIndex]==T){
+            console.log('hi')
             finished=true
-            tIndex=splitIndex
+            tIndex=start+splitIndex
              
         }
-        else if( arr[splitIndex ]<T){
-            dp(arr.slice(0,splitIndex))
+        else if( A[start+splitIndex]<T){
+            if(splitIndex+1==start){
+                return
+            }
+            dp(start+splitIndex+1,end)
+            
             
         }
-        else(
-            dp(arr.slice(splitIndex,arr.length))
-        )
+        else{
+            if(splitIndex-1==end){
+                return
+            }
+            dp(start,start+splitIndex-1)
+            
+        }
+        
         return
 
     }
+
+    dp(0,A.length-1)
+    console.log('finalized tIndex:',tIndex)
+
     var endIndex=new Number(tIndex)
-    while(A[tIndex]==T){
-        tIndex--
+    if(tIndex!=-1){
+        while(A[tIndex]===T){
+            tIndex--
+        }
+        while(A[endIndex]===T){
+            endIndex++
+        }
+     result[0]=tIndex+1
+     result[1]=endIndex-1
     }
-    while(A[endIndex]==T){
-        tIndex++
-    }
-    result[0]=tIndex+1
-    result[1]=endIndex-1
-    
+   
+
+    return result
 };
+
+console.log(searchRange(
+   // [-99999,-99998,-9999,-999,-99,-9,-1],0
+   //[2,2],3
+  //[1],1
+   //[5,7,7,8,8,10],6
+    ))
