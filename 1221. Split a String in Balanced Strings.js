ddf08f642fe938f25ae6a7594b5a0c2ed8e86cc5
GeorgeChryso <lsgechrys@gmail.com>
2019-10-26 18:16:41 +0300

    1221 solved

---
+++ 1221. Split a String in Balanced Strings.js
@@ -0,0 +1,23 @@
+// Balanced strings are those who have equal quantity of 'L' and 'R' characters.
+
+// Given a balanced string s split it in the maximum amount of balanced strings.
+
+// Return the maximum amount of splitted balanced strings.
+
+
+var balancedStringSplit = function(S) {
+    var counter=0
+    var balance=0
+
+    for (let i = 0; i < S.length; i++) {
+            if(S[i]=='L'){
+                balance++
+            }        
+            else{
+                balance--
+            }
+            if( !balance)counter++
+    }
+
+    return counter
+};
