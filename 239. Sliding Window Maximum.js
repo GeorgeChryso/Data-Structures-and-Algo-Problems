0e5c5b8b92b9e5d98867a73a6afe8f55d81d2273
GeorgeChryso <lsgechrys@gmail.com>
2019-10-29 15:37:41 +0200

    239 optimized

--- 239. Sliding Window Maximum.js
+++ 239. Sliding Window Maximum.js
@@ -1,62 +1,27 @@
 // Given an array nums, there is a sliding window of size k which is moving from the very left of the array to the very right. You can only see the k numbers in the window. Each time the sliding window moves right by one position. Return the max sliding window.
 
-function maxSlidingWindow(nums, k) {
-    const res = [];
-    const q = [];
+const maxSlidingWindow = (nums, k) => {
+
+    // Store the max values for each window position
+    let maxes = [];
+    // Queue to determine maxes
+    let q = [];
   
-    for (let i = 0; i < nums.length; i++) {
-      while (q.length - 1 >= 0 && nums[i] > q[q.length - 1]) q.pop();
-      q.push(nums[i]);
-  
-      // When i + 1 - k >= 0, the window is fully overlapping nums
-      const j = i + 1 - k;
-      if (j >= 0) {
-        res.push(q[0]);
-        if (nums[j] === q[0]) q.shift();  // If the biggest element in q is about to exit window, remove it from q
-      }
+    // Iterate through all window positions
+    for (let last = 0, first = 1 - k; last < nums.length; last++, first++) {
+      // Make sure q will stay in descending order after adding new window el
+      while (q.length && nums[last] > q[q.length - 1]) { q.pop(); }
+      // Add new window el to q, gauranteed to be smallest in q
+      q.push(nums[last]);
+      // If window isn't fully overlapping nums, don't yet know the first max
+      if (first < 0) { continue; }
+      // Add q[0] to output, since it's current largest el in window
+      maxes.push(q[0]);
+      // If biggest el in q is about to exit window, remove it from q
+      if (nums[first] === q[0]) { q.shift(); }
     }
-    return res;
+  
+    return maxes;
+      
   }
 
-
-  var maxSlidingWindow = function(nums, k) {
-    if (!nums.length) return [];
-    const findMaxWithIndex = (start, end) => {
-        if (start === end) {
-            return [nums[start], start];
-        } else if (start > end) {
-            return [];
-        }
-        
-        let max = [nums[start], start];
-        
-        for (let i = start + 1; i <= end; i++) {
-            max = nums[i] > max[0] ? [nums[i], i] : max;
-        }
-        
-        return max;
-    };
-    
-    let start = 0;
-    let end = k - 1;
-    let max = null;
-    let maxIndex = null;
-    let result = [];
-    
-    while (end < nums.length) {
-        if (max === null || start > maxIndex) {
-            const newMax = findMaxWithIndex(start, end);
-            max = newMax[0];
-            maxIndex = newMax[1];
-        } else if (nums[end] > max) {
-            max = nums[end];
-            maxIndex = end;
-        }
-        
-        start++;
-        end++;
-        result.push(max);
-    }
-    
-    return result;
-};
