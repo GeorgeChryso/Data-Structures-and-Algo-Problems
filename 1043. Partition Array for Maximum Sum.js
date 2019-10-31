302afd59278d541c95a1050b222eabf67c31f4da
GeorgeChryso <lsgechrys@gmail.com>
2019-10-31 12:33:12 +0200

    1043 Will be posted

--- 1043. Partition Array for Maximum Sum.js
+++ 1043. Partition Array for Maximum Sum.js
@@ -14,31 +14,6 @@
 
 
 
-
-// function maxSumAfterPartitioning( A,  K) {
-//     var N = A.length,
-//         dp=new Array(N).fill(0) // edw apothi
-
-//     for (let i = 0; i < N; i++) {
-//         var curMax = 0;
-//         // tha dokimasw na antikatastisw ola ta pithana k me ton arithmo poy thelw
-//         console.log('i='+i,'dp= ['+dp+']    A= ['+A+']')
-//         for (let k = 1; k <= K && i - k + 1 >= 0; k++) {
-//                       console.log('   k='+k+'\n'+'    dp= ['+dp+']' +'\n' )
-//                       console.log('    curMax= Max('+curMax+', A['+(i+k-1)+']='+A[i-k+1]+') = '+Math.max(curMax, A[i - k + 1])+'\n')
-
-//             curMax = Math.max(curMax, A[i - k + 1]);
-        
-//                     console.log('   dp['+i+']= Max('+dp[i]+','+(i-k >=0 ? dp[i - k] : 0)+'+curMax*k ('+curMax*k+')) = '+ Math.max(dp[i],
-//                         (i-k >=0 ? dp[i - k] : 0) + curMax * k)+'\n' )
-//             dp[i] = Math.max(dp[i],
-//                  (i-k >=0 ? dp[i - k] : 0) + curMax * k);
-//                       console.log('    dp= ['+dp+']'   +'\n   -----------------')
-//         }
-//     }
-//     return dp[N - 1];
-// }
-
 var maxSumAfterPartitioning = function(A, K) {
     let dp =  Array(A.length+1).fill(0);
 
@@ -63,21 +38,42 @@ var maxSumAfterPartitioning = function(A, K) {
 }; 
 
 
-// var maxSumAfterPartitioning = function(a, k) {    
-//     let dp = a.slice(); // a copy of the array
-//     dp.push(0); 
-//     for (let i = a.length-1; i--; ) {
-//         let best = -1;
-//         let max = a[i];
 
-//         for (let j = i, end = Math.min(i+k, a.length); j < end; j++) {
-//             max = Math.max(max, a[j]);
-//             best = Math.max(best, max*(j-i+1) + dp[j+1]);
-//         }
-//         dp[i] = best;
-//     }
-//     return dp[0];
-// };
+var maxSumAfterPartitioning = function(A, K) {
+    let dp =  Array(A.length+1).fill(0);
+
+
+    for (var i in dp) {
+
+        let maximum=0
+        let stop=Math.min(i,K) // I can move back until i-stop elements
+
+
+        for (let j = 1 ; j <=stop ; j++) {
+
+                maximum=Math.max(maximum, A[i-j])
+                
+                dp[i] = Math.max(dp[i], dp[i - j] + maximum * j);
+        }
+
+
+
+    }
+    function dp(i){
+        if(i==0)return A[i]
+        if(i==1)return Math.max(A[0]+A[1],A[1]*2,A[0]*2)
+
+
+        let max=0
+        
+    }
+    return dp[A.length];
+}; 
+
+
+
+
+
 console.log(
     maxSumAfterPartitioning(
         [5,2,7,8,3,1,9],3
