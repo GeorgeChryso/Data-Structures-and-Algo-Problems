5cd8d69c8efea701f3661dc349212c95fdde122e
GeorgeChryso <lsgechrys@gmail.com>
2019-10-31 12:45:54 +0200

    1043 commenting

--- 1043. Partition Array for Maximum Sum.js
+++ 1043. Partition Array for Maximum Sum.js
@@ -12,63 +12,54 @@
 // Output: 84
 // Explanation: A becomes [15,15,15,9,10,10,10]
 
-
-
+//this is dp without memo, maximum call stack exceeded
 var maxSumAfterPartitioning = function(A, K) {
-    let dp =  Array(A.length+1).fill(0);
 
-
-    for (var i in dp) {
-
-        let maximum=0
-        let stop=Math.min(i,K) // I can move back until i-stop elements
-
-
-        for (let j = 1 ; j <=stop ; j++) {
-
-                maximum=Math.max(maximum, A[i-j])
-                
-                dp[i] = Math.max(dp[i], dp[i - j] + maximum * j);
-        }
-
-
-
-    }
-    return dp[A.length];
-}; 
-
-
-
-var maxSumAfterPartitioning = function(A, K) {
-    let dp =  Array(A.length+1).fill(0);
-
-
-    for (var i in dp) {
-
-        let maximum=0
-        let stop=Math.min(i,K) // I can move back until i-stop elements
-
-
-        for (let j = 1 ; j <=stop ; j++) {
-
-                maximum=Math.max(maximum, A[i-j])
-                
-                dp[i] = Math.max(dp[i], dp[i - j] + maximum * j);
-        }
-
-
-
-    }
     function dp(i){
         if(i==0)return A[i]
-        if(i==1)return Math.max(A[0]+A[1],A[1]*2,A[0]*2)
+        if(i==1)return Math.max( Math.max(A[0],A[1])*2,A[0]+A[1])
+      
+        return Math.max(...(()=>{
+            var arr=[]
+           for (let j = 1; j <K; j++) {
+                arr.push(dp(i-j)+Math.max(A[i],A[i-j])*j)               
+           }
+            return arr
+        })())
 
 
-        let max=0
         
+    }
+    return dp(A.length-1);
+}; 
+
+
+
+//this is dp with memo.
+var maxSumAfterPartitioning = function(A, K) {
+    let dp =  Array(A.length+1).fill(0); // dp[i] contains the BEST sum I can have up until index i 
+
+
+    for (var i in dp) {
+
+        let maximum=0
+        let stop=Math.min(i,K) // I can move back until i-stop elements
+
+
+        for (let j = 1 ; j <=stop ; j++) {
+
+                maximum=Math.max(maximum, A[i-j])
+                
+                dp[i] = Math.max(dp[i], dp[i - j] + maximum * j);
+        }
+
+
+
     }
     return dp[A.length];
 }; 
+
+
 
 
 
@@ -76,7 +67,8 @@ var maxSumAfterPartitioning = function(A, K) {
 
 console.log(
     maxSumAfterPartitioning(
-        [5,2,7,8,3,1,9],3
+        [1,15,7,9,2,5,10],
+3
     )
 )
 
