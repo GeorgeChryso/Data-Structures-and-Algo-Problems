0acc28b46a160060749b9f323399c85bf4f2bf01
GeorgeChryso <lsgechrys@gmail.com>
2019-11-06 13:36:18 +0200

    643 Sliding, commenting

--- 643. Maximum Average Subarray I.js
+++ 643. Maximum Average Subarray I.js
@@ -3,15 +3,22 @@
 // 1 <= k <= n <= 30,000.
 // Elements of the given array will be in the range [-10,000, 10,000].
 
+
+// Sliding Window, R:O(N), SP:O(1)
 var findMaxAverage = function(A, K) {
     var result=-Infinity
     var currSum=0
-    for (let i = 0; i < K; i++) {
-        currSum+=A[i]
+    //find the sum of the first window, of size K
+    for (let end= 0; end< K; end++) {
+        currSum+=A[end]
     }
+    //set it as Max
     result=Math.max(currSum/K,result)
-    for (let i = K; i < A.length; i++) {
-        currSum+=A[i]-A[i-K]
+
+    //keep moving the window rightwards
+    for (let end = K; end < A.length; end++) {
+        // adding the new element and subtracting the removed one (the old start of the window)
+        currSum+=A[end]-A[end-K]
         result=Math.max(currSum/K,result)
     }
 
