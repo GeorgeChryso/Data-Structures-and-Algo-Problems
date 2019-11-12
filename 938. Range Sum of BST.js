686080800125954d48c3fad1a891e4ea9187c3d6
GeorgeChryso <lsgechrys@gmail.com>
2019-11-12 23:09:33 +0200

    938 solved

---
+++ 938. Range Sum of BST.js
@@ -0,0 +1,35 @@
+// Given the root node of a binary search tree, return the sum of values of all nodes with value between L and R (inclusive).
+
+// The binary search tree is guaranteed to have unique values.
+
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} L
+ * @param {number} R
+ * @return {number}
+ */
+var rangeSumBST = function(root, L, R) {
+    
+    result=0
+
+    var dp=(node)=>{
+        if(node.left===node.right && node.right===null)return
+        if(node!==null && node.val<=R && node.val>=L )result+=node.val
+
+        dp(node.left)
+        dp(node.right)
+    }
+
+    dp(root)
+    return result
+    
+};
