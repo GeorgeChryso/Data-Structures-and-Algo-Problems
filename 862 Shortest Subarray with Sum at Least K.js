3bd24774434af6e97a498866b32885f69eee8850
GeorgeChryso <lsgechrys@gmail.com>
2019-11-16 20:35:12 +0200

    862 Dequeue Optimized

--- 862 Shortest Subarray with Sum at Least K.js
+++ 862 Shortest Subarray with Sum at Least K.js
@@ -2,49 +2,72 @@
 
 // If there is no non-empty subarray with sum at least K, return -1.
 
+// Would work with only positives
+var shortestSubarray = (A, K) => {
+  var currBestLength = Infinity;
+  var currSum = 0;
+  var start = 0;
 
+  for (let end = 0; end < A.length; end++) {
+    currSum += A[end];
 
-var shortestSubarray = function(A, K) {
-    
-    var result=Infinity
-    var currSum=0
-    var currBestLength=Infinity
-    var start=0
-
-    for (let end = 0; end <  A  .length; end++) {
-    
-            currSum+=A[end]
-
-            if(currSum>=K){
-                currBestLength=Math.min(end-start+1,currBestLength)
-                result=Math.min(result,currBestLength)
-                let tempSum=currSum
-                while (tempSum>=K&&start<end) {
-                    tempSum-=A[start]
-                    start++
-                    if(tempSum>=K){
-                        currSum=tempSum
-                        currBestLength=Math.min(end-start+1,currBestLength)
-                        result=Math.min(result,currBestLength)
-
-                    }
-                    else{
-                        start--
-                        break;
-                    }
-                }
-
-            }
-        
+    while (currSum >= K) {
+      console.log(start, end);
+      currBestLength = Math.min(currBestLength, end - start + 1);
+      currSum -= A[start];
+      start++;
     }
-    
-    return result==Infinity?-1:result
+  }
+
+  return currBestLength == Infinity ? -1 : currBestLength;
 };
 
-console.log(shortestSubarray(
-   // [1,2],4
-   // [1],1
- //  [2,-1,2], 3
- 
-[84,37,32,40,95],167
-))
+// DEQUEUE  O(N) time and space godLee
+var shortestSubarray = (A, K) => {
+  var P = [];
+  var currSum = 0;
+  var currBestLength = Infinity;
+
+
+  for (var val of A) {
+    currSum+=val
+    P.push(currSum)
+  }
+
+  class dequeue {
+    constructor() {
+      //holds the indices of increasing P[i]
+      this.q = [];
+    }
+
+    push = x => {
+      //mystery
+      while (this.q.length && P[x] - P[this.q[0]] >= K) {
+        currBestLength = Math.min(currBestLength, x - this.q.shift());
+      }
+
+      //keeps the dq increasing
+      while (this.q.length && P[this.q[this.q.length - 1]] >= P[x]) {
+        this.q.pop();
+      }
+      this.q.push(x);
+    };
+  }
+
+  var dq = new dequeue();
+
+  for (let i in P) {
+    dq.push(i);
+  }
+  return currBestLength;
+};
+
+console.log(
+  shortestSubarray(
+    //    [1,2],4 //-1
+    // [1],1 //1
+    // [2,-1,2], 3  //3
+    [84, -37, 32, 40, 95],
+    167 //3
+  )
+);
