76b3d4f5c93a51261bc1866d1fac4f578939221e
GeorgeChryso <lsgechrys@gmail.com>
2019-11-25 13:50:41 +0200

    53 comments

--- 53. Maximum Subarray Sum.js
+++ 53. Maximum Subarray Sum.js
@@ -9,13 +9,6 @@
 
 // If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
 
-var maxSubArray = function(nums) {
-    for (let i = 1; i < nums.length; i++) {
-        nums[i] = Math.max(nums[i], nums[i] + nums[i - 1]);
-    }
-
-    return Math.max(...nums);
-};
 
 // Works O(N),but is overcomplicated , i use prefix sum + a stack to keep track of the minimum prefix sum
 var maxSubArray = A => {
@@ -55,7 +48,6 @@ var maxSubArray = A => {
 // Intuition find a pair (minSum, result)
 // such that the minSum is the leftside minimum prefix Sum of Result, the maximum Prefix Sum of the array
 // Essentially, on each Prefix Sum, I'm searching for the leftmost minmimum element so their difference (PrefixSum - leftminimum) yields the highest result( sum of the corresponding subarray)
-
 var maxSubArray = A => {
     if(!A.length)return 0
     var minSum = A[0];
@@ -69,6 +61,36 @@ var maxSubArray = A => {
     }
 
     return result;
+};
+
+// ok less space then, we can do without PrefixSum array
+var maxSubArray = A => {
+    if(!A.length)return 0
+    var minSum = A[0];
+    var maxSoFar=minSum //old PrefixSum Value
+    var result = minSum;
+
+    for (let i = 1; i < A.length; i++) {
+        maxSoFar+= A[i];
+        result = Math.max(result, maxSoFar - minSum, maxSoFar);
+        minSum = Math.min(minSum, maxSoFar);
+    }
+
+    return result;
+};
+
+// Optimal Kadane's algorithm: runtime O(n) space O(1)
+// Essentially, on every i there are 2 options: Either I start my result subarray from that Index, OR I extend the best subarray so far so i can maximize the sum
+var maxSubArray = function(nums) {
+     // Each position nums[i] will hold either 2 values
+     // 1. Either the Highest sum of the best subarray so far, 
+     // 2. or the new Beginning of a potentially better subarray
+     // I'm asking myself at every index : Should I add the current element to my subarray? Or start a new from this point forward
+    for (let i = 1; i < nums.length; i++) {
+        nums[i] = Math.max(nums[i], nums[i] + nums[i - 1]);
+    }
+
+    return Math.max(...nums);
 };
 
 console.log(
