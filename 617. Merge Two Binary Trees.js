4103c765dd75544b3068ad03ca6a1b0a024f3d16
GeorgeChryso <lsgechrys@gmail.com>
2019-11-26 19:26:43 +0200

    617 prog

--- 617. Merge Two Binary Trees.js
+++ 617. Merge Two Binary Trees.js
@@ -2,7 +2,6 @@
 
 // You need to merge them into a new binary tree. The merge rule is that if two nodes overlap, then sum node values up as the new value of the merged node. Otherwise, the NOT null node will be used as the node of new tree.
 
-
 /**
  * Definition for a binary tree node.
  * function TreeNode(val) {
@@ -17,23 +16,32 @@
  */
 var mergeTrees = function(t1, t2) {
     function TreeNode(val) {
-             this.val = val;
-             this.left = this.right = null;
-         }
-    let answ= new TreeNode
-    
-        
-    var dp=(a)=>{
-        if(a.left===a.right&& a.left===null)return false
-        answ.left=
-        answ.right=
-        answ.val=
-        dp(a.left)
-        dp(a.right)
+        this.val = val;
+        this.left = this.right = null;
     }
-    
-    while(dp(t2)){
+    let answ = new TreeNode();
 
-    }
+    var series = [];
 
+    var dp = (n1, n2) => {
+        if (n1 === null) {
+        }
+        if (n2 === null) {
+        }
+
+        let res = new TreeNode();
+        if (n1.val !== null && n2.val !== null) {
+            res.val = n1.val + n2.val;
+        } else if (n1.val !== null) {
+            res.val = n1.val;
+        } else if (n2.val !== null) {
+            res.val = n2.val;
+        } else {
+            res == null;
+        }
+
+        series.push(res);
+    };
+
+    while (dp(t1, t2)) {}
 };
