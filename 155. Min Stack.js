bf38caa9bcd61f2610166af2b6589e30be6541af
GeorgeChryso <lsgechrys@gmail.com>
2019-11-28 23:11:57 +0200

    155 optimized

--- 155. Min Stack.js
+++ 155. Min Stack.js
@@ -45,3 +45,55 @@ MinStack.prototype.getMin = function() {
  * var param_3 = obj.top()
  * var param_4 = obj.getMin()
  */
+
+
+ //Optimzied with a use of a log
+ /**
+ * initialize your data structure here.
+ */
+var MinStack = function() {
+    this.q=[];
+    this.min=[Infinity]
+};
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MinStack.prototype.push = function(x) {
+ 
+    this.q.push(x)
+    this.min.push(Math.min(this.min[this.min.length-1],x))
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function() {
+    this.q.pop()
+    this.min.pop()
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function() {
+    return this.q[this.q.length-1]
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function() {
+    //return Math.min(...this.q)
+    return this.min[this.min.length-1]
+};
+
+/** 
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(x)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
