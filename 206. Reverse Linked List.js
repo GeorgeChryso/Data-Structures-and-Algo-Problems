b2508bb4d669834945b2faf0c45bdc376b8c9d1a
GeorgeChryso <lsgechrys@gmail.com>
2019-11-29 21:16:52 +0200

    206 2 solutions

--- 206. Reverse Linked List.js
+++ 206. Reverse Linked List.js
@@ -24,22 +24,70 @@
  */
 var reverseList = function(head) {
   
-    var z=[]
 
-    while(head){
-        z.push(head.val)
-        head=head.next
+    var prev = new ListNode(null)
+    var curr = new ListNode(head)
+
+    while( curr!==null){
+        nextTemp= new ListNode(curr.next)
+        curr.next=prev
+        prev=curr;
+        curr= nextTemp;
+
     }
-
     
-
-   let  curr= new ListNode(z.pop())
-        
-
-    var next=(curr)=>{
-        if(!z.length)return 
-        curr.next=new ListNode(z.pop)
-        return curr.next
-    }
-    return next(curr)
+    return prev
 };
+
+var reverseList = function(head){
+  
+    var tmp = null;
+    var newHead = null;
+    while(head !== null){
+      tmp = head;
+      head = head.next;
+      tmp.next = newHead;
+      newHead = tmp;
+    }
+    
+    return newHead;
+  }
+
+
+
+  var reverseList = function(head) {
+    //iteratively use a stack
+    //1. add the node to the stack,
+    //2. go to next node,
+    //3. repeat until no more nodes,
+    //4. then make a new linked list by popping off the stack.
+    
+    if (head === null || head.next === null) return head;
+    
+    let stack = [];
+    
+    while (head) {
+        stack.push(head.val);
+        head = head.next;
+    }
+    
+    let n = new ListNode(stack.pop())
+    let newHead = n;
+    
+    while(stack.length) {
+        n.next = new ListNode(stack.pop());
+        n = n.next;
+    }
+    n.next = null;
+    return newHead;
+};
+
+
+
+var reverseList = function(head) {
+    let [prev, current] = [null, head]
+    while(current) {
+        [current.next, prev, current] = [prev, current, current.next]
+    }
+    return prev
+}
