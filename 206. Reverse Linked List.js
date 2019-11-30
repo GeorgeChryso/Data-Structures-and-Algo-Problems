be74fcef1357b1419bd708081d23351a984f9b5b
GeorgeChryso <lsgechrys@gmail.com>
2019-11-30 20:53:15 +0200

    206 optimized

--- 206. Reverse Linked List.js
+++ 206. Reverse Linked List.js
@@ -87,7 +87,9 @@ var reverseList = function(head){
 var reverseList = function(head) {
     let [prev, current] = [null, head]
     while(current) {
-        [current.next, prev, current] = [prev, current, current.next]
+      //  [current.next, prev, current] = [prev, current, current.next]
+
+        [prev,current.next,current]=[current,prev,current.next]
     }
     return prev
 }
