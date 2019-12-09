8ef44ad1edc8c9201499632a70b05d74ab73afb7
GeorgeChryso <lsgechrys@gmail.com>
2019-12-09 17:26:39 +0200

    94 optimized

---
+++ 94. Binary Tree Inorder Traversal.js
@@ -0,0 +1,26 @@
+// Given a binary tree, return the inorder traversal of its nodes' values.
+
+
+//recursive solution (dfs)
+// Runtime O(n):visit each node only once
+// Length O(n): result
+var inorderTraversal = function(root) {
+    if(!root)return []
+    var result=[]
+    var stack=[]
+
+
+    //this doesnt actually return anything, its just that the way of the inorder traversal fits perfectly with how dfs traverses the tree;
+    var dfs=(node)=>{
+        if(!node){
+            if(stack.length)result.push(stack.pop())
+            return
+        }
+        stack.push(node.val)
+        dfs(node.left)
+        dfs(node.right)
+    }
+    
+    dfs(root)
+    return result
+};
