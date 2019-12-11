4ad8a640febddc228c877796e60b992717012f79
GeorgeChryso <lsgechrys@gmail.com>
2019-12-11 19:43:22 +0200

    touzbouz added

--- 494. Target Sum.js
+++ 494. Target Sum.js
@@ -69,8 +69,14 @@ var findTargetSumWays = function(Arr, S) {
     return dp[S]
 };
 
-
-
+//touzbouz
+var findTargetSumWays = function(nums, S, sum = 0, i = 0, memo = new Map()){
+    let key = sum + ',' + i;
+    if (memo.has(key)) return memo.get(key);
+    if (i === nums.length) return +(sum === S);
+    memo.set(key, findTargetSumWays(nums, S, sum + nums[i], i + 1, memo) + findTargetSumWays(nums, S, sum - nums[i], i + 1, memo));
+    return memo.get(key);
+};
 
 
 
