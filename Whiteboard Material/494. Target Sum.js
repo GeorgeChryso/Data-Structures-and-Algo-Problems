0d40ecdbe847f6cfc194269caa52d3902502cfad
GeorgeChryso <lsgechrys@gmail.com>
2019-12-16 22:23:39 +0200

    dp knap+ memo

--- Whiteboard Material/494. Target Sum.js
+++ Whiteboard Material/494. Target Sum.js
@@ -63,30 +63,39 @@ var findTargetSumWays = function(nums, S, sum = 0, i = 0, memo = new Map()){
 
 //knapsack concept have to write it down
 
+// classic knapsack: Choose the item or not, with a total weight constraint in order to maximize value
+// this knapsack: Add the item or not(subtract it), with a total weight constraint S, in order to maximize the number of ways for first i-th(nth) element to reach a sum+S
+
 var findTargetSumWays = function(Arr, S) {
     
     var sum=Arr.reduce((acc,curr)=>acc+curr)
     if(S<-sum||S>sum)return 0
 
-    var dp=Array(Arr.length+1).fill(null).map(d=>Array(2*sum+1))
+    var dp=Array(Arr.length+1).fill().map(d=>new Array(2*sum+1).fill(0))
 
-    dp[0][0+sum]=1// 0+sum means 0, 0means  -sum
+    dp[0][0+sum]=1 // j=0+sum means 0, j=0 means -sum
     //giati to euros twn lusewn mou einai apo [-sum, sum]
     
-    for (let i = 1; i < Arr.length; i++) {
+    for (let i = 1; i <= Arr.length; i++) {
+        dp.forEach(d=>console.log(d+''))
+
         for (let j = 0; j < (2  * sum + 1); j++) {
-            if((j + Arr[i - 1]) < (2  * sum + 1)) dp[i][j] += dp[i - 1][j + Arr[i - 1]];
+            
+            if((j + Arr[i - 1]) <= 2  * sum ) dp[i][j] += dp[i - 1][j + Arr[i - 1]]
             if((j - Arr[i - 1] )>= 0) dp[i][j] += dp[i - 1][j - Arr[i - 1]];
             
         }        
+        console.log('\n')
     }
+
+    dp.forEach(d=>console.log(d+''))
 
     return dp[Arr.length][sum+S]
 };
 
 
 console.log(findTargetSumWays(
-    [1, 1, 1, 1, 1],3 
+    [1, 2, 1, 3],4 
 ))
 
 
