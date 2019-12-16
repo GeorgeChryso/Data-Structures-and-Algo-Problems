6082ffb3a3e5da225339d51e180f36d9916a7d6c
GeorgeChryso <lsgechrys@gmail.com>
2019-12-16 23:23:32 +0200

    new solution and commenting

--- Whiteboard Material/494. Target Sum.js
+++ Whiteboard Material/494. Target Sum.js
@@ -26,31 +26,7 @@ var findTargetSumWays = function(Arr, S) {
 
 
 
-//?? yuxiangmusic
-var findTargetSumWays = function(Arr, S) {
-    var sum=Arr.reduce((acc,curr)=>acc+curr)
-    
-    // S+sum explanation:
-    // So the problem becomes
-    // Find a subset P of nums such that sum(P) = (target + sum(nums)) / 2
-    // where P is the positives that I choose
-    if((sum<S)||(S+sum)%2)return 0
 
-    //??
-    S=(S+sum)>>1
-
-
-
-    //prolly storing the sums here
-    let dp=Array(S+1).fill(0)
-    dp[0]=1
-    for (const n of Arr) {
-        for (let i = S; i>=n; i--) { 
-            dp[i]+=dp[i-n]            
-        }
-    }
-    return dp[S]
-};
 
 //touzbouz
 var findTargetSumWays = function(nums, S, sum = 0, i = 0, memo = new Map()){
@@ -66,11 +42,16 @@ var findTargetSumWays = function(nums, S, sum = 0, i = 0, memo = new Map()){
 // classic knapsack: Choose the item or not, with a total weight constraint in order to maximize value
 // this knapsack: Add the item or not(subtract it), with a total weight constraint S, in order to maximize the number of ways for first i-th(nth) element to reach a sum+S
 
+
+// That would be O(N*(2*S+1))=O(N*S) runtime and 
+// O(N*(SUM of the elements of the array)) space
+// It can be reduced to O(SUM) space
 var findTargetSumWays = function(Arr, S) {
     
     var sum=Arr.reduce((acc,curr)=>acc+curr)
     if(S<-sum||S>sum)return 0
 
+    // each cell dp[i][j] represents the number of ways I can reach sum equal to j-sum 
     var dp=Array(Arr.length+1).fill().map(d=>new Array(2*sum+1).fill(0))
 
     dp[0][0+sum]=1 // j=0+sum means 0, j=0 means -sum
@@ -94,8 +75,69 @@ var findTargetSumWays = function(Arr, S) {
 };
 
 
+// Here i m turning the space complexity to O(sum) (still using Knapsack memo)
+var findTargetSumWays = function(Arr, S) {
+    
+    var sum=Arr.reduce((acc,curr)=>acc+curr)
+    if(S<-sum||S>sum)return 0
+
+    //reducing the space complexity cos I just need 2 arrays at a time
+    var previous=new Array(2*sum+1).fill(0)
+    var current=new Array(2*sum+1).fill(0)
+
+
+    previous[0+sum]=1 
+    
+    for (let i = 1; i <= Arr.length; i++) {
+        for (let j = 0; j < (2  * sum + 1); j++) {
+
+            //you need to set your new array's element to zero first,cos current keeps its 
+            // "pseudo" state
+            current[j]=0
+
+            if((j + Arr[i - 1]) <= 2  * sum ) current[j] += previous[j + Arr[i - 1]]
+            if((j - Arr[i - 1] )>= 0) current[j] += previous[j - Arr[i - 1]];
+            
+        }
+        //essentially copy the current array to previous
+        previous=[...current]        
+
+    }
+
+
+    return previous[sum+S]?previous[sum+S]:0
+};
+
+// yuxiangmusic
+var findTargetSumWays = function(Arr, S) {
+    var sum=Arr.reduce((acc,curr)=>acc+curr)
+    
+    // S+sum explanation:
+    // So the problem becomes
+    // Find a subset P of nums such that sum(P) = (target + sum(nums)) / 2
+    // where P is the positives that I choose
+    if((sum<S)||(S+sum)%2)return 0
+
+    //??
+    S=(S+sum)>>1
+
+
+
+    //prolly storing the sums here
+    let dp=Array(S+1).fill(0)
+    dp[0]=1
+
+    for (const weight of Arr) {
+        for (let possible_weight = S; possible_weight>=weight; possible_weight--) { 
+            dp[possible_weight]+=dp[possible_weight-weight]            
+        }
+    }
+    return dp[S]
+};
+
 console.log(findTargetSumWays(
-    [1, 2, 1, 3],4 
+    //[1, 2, 1, 3],4 
+    [1,1,1,1,1],3
 ))
 
 
