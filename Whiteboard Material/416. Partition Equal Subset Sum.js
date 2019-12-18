cacdad6a03a25c40846b2370f847128a9d185e4e
GeorgeChryso <lsgechrys@gmail.com>
2019-12-18 18:19:23 +0200

    solving

--- Whiteboard Material/416. Partition Equal Subset Sum.js
+++ Whiteboard Material/416. Partition Equal Subset Sum.js
@@ -30,5 +30,30 @@
 // plus
 // the number of ways I can reach the same sum minus the sum of the i-1th item , which means that I chose the last item in order to get to my sum J 
 var canPartition = function(A) {
+    //calculate the sum of my Array
+    var sumA=A.reduce((acc,curr)=>acc+curr)
+
+    if(sumA%2)return false
+
+    //create Rows
+    // i want a row for each of my candidate elements+ one for my 
+    // 0th element( no element ) which I know for a fact can add up to 0 if selected
+    var dp=new Array(A.length+1).fill(null)
     
+    // create Columns
+    // My final total sum ranges from 0 to sumA, which are totally sumA+1 candidates
+    dp=dp.map(d=>Array(sumA+1).fill(0))
+    
+    // now that the matrix is created i have to use my base case which is: 
+    // The number of ways I can end up with sum=0 by using 0 items is 1 ways: just by selecting the 0th item (doesnt exist, which means i just take no item at all)
+     dp[0][0]=1
+
+     //now let's see what I actaully want to find
+     //if there is ANY subset, that adds Up to sumA/2
+     //so that would mean ANY element of the column A/2, that would be dp[;][A/2]
+
+     //now, theoretically, I could fill the whole board and then check my column but that's BOOOOOOOOOOORING
+     // so let's look at my formula again
+     // dp[i][j]=dp[i-1][j]+dp[i-1][j-A[i-1]]
+
 };
