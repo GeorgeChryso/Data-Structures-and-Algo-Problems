51a3f45d432e15c7d7885adecb6b199889c2d254
GeorgeChryso <lsgechrys@gmail.com>
2019-12-19 19:28:09 +0200

    added a bit solution, needs commenting

--- Whiteboard Material/416. Partition Equal Subset Sum.js
+++ Whiteboard Material/416. Partition Equal Subset Sum.js
@@ -62,7 +62,7 @@ var canPartition = function(A) {
     // so It's ok not to fill the whole board and just go up to my desired column (A/2)
 
     //here i=0 cos everything other column (sum) of this row cannot be created with 0 elements
-    for (let i = 1; i < dp.length; i++) {
+    for (let i = 1; i < A.length; i++) {
         for (let j = 0; j <= sumA / 2; j++) {
             //I know that i-1>=0 so i dont need an extra check for that
             dp[i][j] += dp[i - 1][j];
@@ -83,15 +83,17 @@ var canPartition = function(A) {
 
     if (sumA % 2) return false;
 
-    var previous = new Array(sumA / 2 + 1).fill(0);
-    //with the new way I end up not needing as second array
+    var previous = new Array((sumA / 2) + 1).fill(0);
+
+
+    //with the new way I end up not needing as second array (LOOK further down)
         //var current = new Array(sumA / 2 + 1).fill(0);
         //or if youre kewl
         // var current=[...previous] // copies the array, same as
         // var current=Array.from(previous) //l8am8a
     previous[0] = 1;
 
-    for (let i = 1; i < previous.length; i++) {
+    for (let i = 1; i < A.length; i++) {
 
         //OLD WAY ,with hard copying the previous array
                 // for (let j = 0; j <= sumA / 2; j++) {
@@ -114,13 +116,23 @@ var canPartition = function(A) {
         // New way, better runtime and space cos i m not hard copying the array
             //ok it's actually big brain time, what if I only use just one array and traverse it from right to left HUUUUUUUUH? what if I just use previous and just update it leftwards in order for me to not alter the important columns
             for (let j = sumA/2; j >=0; j--){
+
                 previous[j]+=(j - A[i - 1] >= 0)?previous[j - A[i - 1]]:0 
+
                 if (j == sumA / 2 && previous[j]) return true;
             }
-            // inb4 it actually works and its better(O(sumA+1 constant space))
+         // inb4 it actually works and its better(O(sumA+1)) space
     }
 
     return false;
+};
+
+
+// Best optimized way using BITS
+var canPartition = function(nums){
+    const bits = nums.reduce((acc, num) => acc | acc << BigInt(num),1n)
+    const acc = nums.reduce((acc, num) => acc + num)
+    return !(acc & 1) && bits >> BigInt(acc >> 1) & 1n
 };
 
 console.log(canPartition([1, 5, 11, 5]));
