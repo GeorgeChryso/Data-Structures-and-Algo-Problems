e7571c4ff3a13b447535a2ccde85d1fadd0cbc65
GeorgeChryso <lsgechrys@gmail.com>
2020-01-08 20:54:49 +0200

    931 recursive soltuion added

--- 931. Minimum Falling Path Sum.js
+++ 931. Minimum Falling Path Sum.js
@@ -25,11 +25,34 @@ var minFallingPathSum = function(A) {
 
     for (let i = A.length-2; i>=0; i--) {
         for (let j = 0; j < A[0].length; j++) {
-            A[i][j]=Math.min(A[i+1][j],j+1<=A[0].length-1?A[i+1][j+1]:Infinity,j>=1?A[i+1][j-1]:Infinity)+A[i][j]
+            A[i][j]=Math.min(
+                A[i+1][j],                              //either same column
+                j+1<=A[0].length-1?A[i+1][j+1]:Infinity, // or next( if able)
+                j-1>=0?A[i+1][j-1]:Infinity) // or previous( if able)
+                +A[i][j]
         }
     }
     return Math.min(...A[0])
 };
+
+var minFallingPathSum=A=>{
+    let result=Infinity
+    let dp=Array(A.length).fill(null).map(d=>Array(A[0].length).fill(null))
+    // dp[i][j] the minimum distance from the beginning to A[i][j]
+    dp[0]=A[0] // base case 
+    let recursion=(i,j)=>{
+        if(i>=A.length||j<0||j>=A[0].length)return Infinity
+        if(dp[i][j]!==null)return dp[i][j]
+        dp[i][j]= Math.min(recursion(i+1,j),recursion(i+1,j-1),recursion(i+1,j+1))+A[i][j]
+        return dp[i][j]
+    }
+    A[0].forEach( (el,j) => {
+        recursion(0,j)        
+    });
+    console.log(dp)
+    return Math.min(...dp[A.length-1])
+}
+
 
 console.log(
     minFallingPathSum(
