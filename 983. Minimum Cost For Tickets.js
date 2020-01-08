8417b6f0cf15ac8e8c1e189270031baadc4705b7
GeorgeChryso <lsgechrys@gmail.com>
2020-01-08 21:35:40 +0200

    new problem

---
+++ 983. Minimum Cost For Tickets.js
@@ -0,0 +1,19 @@
+// In a country popular for train travel, you have planned some train travelling one year in advance.  The days of the year that you will travel is given as an array days.  Each day is an integer from 1 to 365.
+
+// Train tickets are sold in 3 different ways:
+
+// a 1-day pass is sold for costs[0] dollars;
+// a 7-day pass is sold for costs[1] dollars;
+// a 30-day pass is sold for costs[2] dollars.
+// The passes allow that many days of consecutive travel.  For example, if we get a 7-day pass on day 2, then we can travel for 7 days: day 2, 3, 4, 5, 6, 7, and 8.
+
+// Return the minimum number of dollars you need to travel every day in the given list of days.
+
+
+var mincostTickets = function(days, costs) {
+    let [price1Day,price7Days,price30Days]=costs
+    
+    let recursion=(index,cost,daysLeft)=>{
+        
+    }
+};
