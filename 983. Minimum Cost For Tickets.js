95aadcbe638f4661c8aa6a0004d54eccaecf8c5f
GeorgeChryso <lsgechrys@gmail.com>
2020-01-12 17:29:42 +0200

    983 dp progress

--- 983. Minimum Cost For Tickets.js
+++ 983. Minimum Cost For Tickets.js
@@ -10,10 +10,38 @@
 // Return the minimum number of dollars you need to travel every day in the given list of days.
 
 
-var mincostTickets = function(days, costs) {
-    let [price1Day,price7Days,price30Days]=costs
-    
-    let recursion=(index,cost,daysLeft)=>{
-        
+
+//dfs?
+var mincostTickets=(A,costs)=>{
+    let cost
+
+}
+
+var mincostTickets = function(A, costs) {
+    let days=price=>{
+        if(price==costs[0])return 1
+        if(price==costs[1])return 7
+        if(price==costs[2])return 30
     }
+    let dp=Array(386).fill(Infinity)
+    let memodays=new Set(A)
+    //dp[i]
+
+    //base case 
+    dp[0]=0 // need 0 dollars to travel to day 0
+
+   
+    for (let i = 1; i <=365; i++) {
+            //BUY ONLY ON DAYS THAT U HAVE TO TRAVEL OR DONT BUY AT ALL
+            if(memodays.has(i)){
+                costs.forEach(cost => dp[i]=Math.min(dp[i+days(cost)]+cost,dp[i]));
+            }
+    }
+    console.log(dp+'')
+    return Math.min(...dp.slice(365))
 };
+
+
+console.log(mincostTickets(
+    [1,4,6,7,8,20],  [2,7,15]
+))
