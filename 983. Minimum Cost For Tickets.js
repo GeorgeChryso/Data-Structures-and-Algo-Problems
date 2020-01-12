fd50624ea57e64ef3f98e3f9ebf75280e67034bb
GeorgeChryso <lsgechrys@gmail.com>
2020-01-12 19:12:35 +0200

    more solutions

--- 983. Minimum Cost For Tickets.js
+++ 983. Minimum Cost For Tickets.js
@@ -80,8 +80,13 @@ var mincostTickets=(A,costs)=>{
     let last30=[]
 
     for (const day of A) {
-        while(last7.length && (last7[0][0]+7)<=day)last7.pop()
-        while(last30.length &&(last30[0][0]+30)<=day)last30.pop()
+
+        //discard all the expired 7 days passes
+        while(last7.length && (last7[0][0]+7)<=day)last7.shift()
+        // and all the expired 30 days passes
+        while(last30.length &&(last30[0][0]+30)<=day)last30.shift()
+
+        //pushing the possibility of buying a 7 and 30 pass on
         last7.push([day,cost+costs[1]])
         last30.push([day,cost+costs[2]])
         cost=Math.min( cost+costs[0],last7[0][1],last30[0][1])
@@ -90,6 +95,41 @@ var mincostTickets=(A,costs)=>{
 
     return cost
 }
+
+//?? needs studying
+var mincostTickets = function(days, costs) {
+    // dynamic problem
+    // each new day have 3 options
+    // 1) add another single day pass
+    // 2) add 7 day pass
+    // 3) add 30 day pass
+    // Find the  min of all three options
+    
+    
+    let findCost = function(ticketDay, index, days, totals) {
+        // find the cost since day
+        let prevDay = days[index] - ticketDay
+        
+        for(let i = index-1; i >= 0; i--) {
+            if(days[i] <= prevDay)  return totals[i]
+        }
+        
+        return 0
+    }
+
+    let totals = []
+    
+    for(let i in days) {
+        let cost1 = (i == 0? 0: totals[i-1]) + costs[0]
+        let cost7 = findCost(7, i, days, totals) + costs[1]
+        let cost30 = findCost(30, i, days, totals) + costs[2]
+        
+        totals[i] = Math.min(cost1, cost7, cost30)
+        // console.log({i: i, cost1: cost1, totals: totals})
+    }
+    return totals[totals.length-1]
+};
+
 console.log(mincostTickets(
  //   [1,4,6,7,8,20],[2,7,15]
     [4,5,9,11,14,16,17,19,21,22,24],
