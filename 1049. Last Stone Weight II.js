a944c94707e8f5dae0df79833e53e2d35ed7f0b1
GeorgeChryso <lsgechrys@gmail.com>
2020-01-15 19:01:43 +0200

    commenting and modifications

--- 1049. Last Stone Weight II.js
+++ 1049. Last Stone Weight II.js
@@ -11,27 +11,49 @@
 
 // essentially This problem asks me to place my elements into two groups, 
 // one with + before them and one with -. Essentially forming my final sum (result). 
-
+// sum(P)-sum(N)=result
+// total=result +sum(N)
+// sum(N)=total-result
 
 var lastStoneWeightII=A=>{
     // There is a constraint regarding the weights. 
     // There cannot be any negative weight therefore my knapsack
     // will have 0 as its lowest possible sum.
+    // Update: Although there cannot be any negative weight, my final answer depends on temporary given sums. For example if  i somehow combine  my first n-1 items and they sum up to -7 and my final item is 8, it needs the information that -7 was possible.
+
     let total=A.reduce((a,b)=>a+b)
-    let dp=Array(total+1).fill(0)
+
+    // Therefore i need any possible sum that can be achieved.
+    // That would be [-total,+total] 
+    let dp=Array(2*total+1).fill(0)
     //dp[i] means that sum i is possible through a combination of my elements
-    dp[0]=1
+    // dp[total+1] means that sum 0 is possible while using zero items.
+    // here: total+1 refers to sum 0
+    dp[total+1]=1
 
     for (let i = 0; i < A.length; i++) {
-        for (let j = 0; j < dp.length; j++) {
-            if(j-A[i]>=0)dp[j]= dp[j-A[i]]||dp[j]
-            if(j+A[i]<dp.length)dp[j]=dp[j]||dp[j+A[i]]            
-        }        
+        dp=dp.map( (el,j)=>{
+            let result=0
+            //result j can only be achieved either by adding the current adding the current element
+            if(j-A[i]>=0)result=  dp[j-A[i]]||result
+            // or by subtracting it to get that result
+            if(j+A[i]<dp.length)result= result||dp[j+A[i]]       
+            return result
+        })
     }
-    
 
 
-    return dp.indexOf(1)==-1?0:dp.indexOf(1)
+    // As i said, i m only interested in positive results. Therefore I m searching for the index of the first 1, the smallest possible sum
+  
+
+    for (let i = total+1; i < dp.length; i++) {
+        if(dp[i])return i-total-1       
+    }
+    return 0
+
+
+     // dp=dp.slice(total+1)
+   // return dp.indexOf(1)==-1?0:dp.indexOf(1)
 }
 
 
