17d6fbba33646954d7e0d1c9a127306cf0ff9678
GeorgeChryso <lsgechrys@gmail.com>
2020-01-15 18:17:11 +0200

    last stone weight dp +- solution

--- 1049. Last Stone Weight II.js
+++ 1049. Last Stone Weight II.js
@@ -7,17 +7,34 @@
 // At the end, there is at most 1 stone left.  Return the smallest possible weight of this stone (the weight is 0 if there are no stones left.)
 
 
-// naive recursion
-var lastStoneWeightII = function(A) {
-    let total=A.reduce((acc,curr)=>acc+curr)
-    let dp=Array(A.length+1).fill(null).map(d=>Array(A.length+1).fill(total))
+// spot the knapsack
+
+// essentially This problem asks me to place my elements into two groups, 
+// one with + before them and one with -. Essentially forming my final sum (result). 
+
+
+var lastStoneWeightII=A=>{
+    // There is a constraint regarding the weights. 
+    // There cannot be any negative weight therefore my knapsack
+    // will have 0 as its lowest possible sum.
+    let total=A.reduce((a,b)=>a+b)
+    let dp=Array(total+1).fill(0)
+    //dp[i] means that sum i is possible through a combination of my elements
+    dp[0]=1
 
     for (let i = 0; i < A.length; i++) {
-        for (let j = 0; j < A.length; j++) {
-                if(j!=i){
-
-                }          
+        for (let j = 0; j < dp.length; j++) {
+            if(j-A[i]>=0)dp[j]= dp[j-A[i]]||dp[j]
+            if(j+A[i]<dp.length)dp[j]=dp[j]||dp[j+A[i]]            
         }        
     }
     
-};
+
+
+    return dp.indexOf(1)==-1?0:dp.indexOf(1)
+}
+
+
+console.log(lastStoneWeightII(
+    [2,7,4,1,8,1]
+))
