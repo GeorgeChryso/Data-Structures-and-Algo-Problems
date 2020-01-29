a23269348fa73cf8a27d020fd8584520bf4f8238
GeorgeChryso <lsgechrys@gmail.com>
2020-01-29 22:57:07 +0200

    knapsack commenting;

--- 474. Ones and Zeroes.js
+++ 474. Ones and Zeroes.js
@@ -5,7 +5,8 @@
 // Now your task is to find the maximum number of strings that you can form with given m 0s and n 1s. Each 0 and 1 can be used at most once.
 
 
-
+// It is a knapsack, I did found the dp connectio but I get tle due to 
+// the hash I use
 var findMaxForm = function(strs, m, n) {
     for (const i in strs) {
         let container=[0,0]
@@ -15,35 +16,36 @@ var findMaxForm = function(strs, m, n) {
         strs[i]=[...container]
     }
     
-    let possibleCases={
-        
-    }
+    let possibleCases={}
     for (let i = 0; i <=m; i++) {   
         for (let j = 0; j <=n; j++) {
             possibleCases[[i,j]]=Infinity           
         }        
     }
     let dp=Array(strs.length+1).fill(null).map(d=>Object.assign({},possibleCases))
+    // dp[i][[k,l]] is the max number of items i can take while having used [k,l] 0,1s
+    //basecase
     Object.keys(dp[0]).forEach(d=>dp[0][d]=0)
     for (let i = 1; i < dp.length; i++) {
            let [mm,nn]=strs[i-1]
            for (const dora of Object.keys(dp[i])) {
                 let [x,y]=dora.split(',')
            
-            if(x>=mm&&y>=nn){
-                if(dp[i-1][[x,y]]===Infinity){
-                    dp[i][[x,y]]=1+dp[i-1][[x-mm,y-nn]]
-                    continue
-                }
-                if(dp[i-1][[x-mm,y-nn]]===Infinity){
-                    dp[i][[x,y]]=dp[i-1][[x,y]]
-                    continue
-                }
-                dp[i][[x,y]]=Math.max(dp[i-1][[x,y]],1+dp[i-1][[x-mm,y-nn]])
+                if(x>=mm&&y>=nn){
+                    if(dp[i-1][[x,y]]===Infinity){
+                        dp[i][[x,y]]=1+dp[i-1][[x-mm,y-nn]]
+                        continue
+                    }
+                    if(dp[i-1][[x-mm,y-nn]]===Infinity){
+                        dp[i][[x,y]]=dp[i-1][[x,y]]
+                        continue
+                    }
+                    dp[i][[x,y]]=Math.max(dp[i-1][[x,y]],1+dp[i-1][[x-mm,y-nn]])
 
-            }
-            else dp[i][[x,y]]=dp[i-1][[x,y]]
+                }
+                else dp[i][[x,y]]=dp[i-1][[x,y]]
            }
+           console.log(dp[i])
     }   
     return Object.values(dp[dp.length-1]).reduce((acc,curr)=>{
        if(curr===Infinity)return acc
@@ -53,6 +55,6 @@ var findMaxForm = function(strs, m, n) {
 
 
 console.log(findMaxForm(
-   // ["10","0001","111001","1","0"],5,3
-    ["10", "0", "1"],1,1
+    ["10","0001","111001","1","0"],5,3
+  //  ["10", "0", "1"],1,1
 ))
