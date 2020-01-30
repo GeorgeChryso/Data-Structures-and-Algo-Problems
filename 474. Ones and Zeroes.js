b854eaabbb0194f48af0b258715ad2d7e0e1bc22
GeorgeChryso <lsgechrys@gmail.com>
2020-01-30 19:38:41 +0200

    removed deprecated solution

--- 474. Ones and Zeroes.js
+++ 474. Ones and Zeroes.js
@@ -5,75 +5,41 @@
 // Now your task is to find the maximum number of strings that you can form with given m 0s and n 1s. Each 0 and 1 can be used at most once.
 
 
-// It is a knapsack, I did found the dp connectio but I get tle due to 
-// the hash I use
-var findMaxForm = function(strs, m, n) {
-    for (const i in strs) {
-        let container=[0,0]
-        for (const bit of strs[i]) {
-            container[Number(bit)]++
-        }
-        strs[i]=[...container]
-    }
-    
-    let possibleCases={}
-    for (let i = 0; i <=m; i++) {   
-        for (let j = 0; j <=n; j++) {
-            possibleCases[[i,j]]=Infinity           
-        }        
-    }
-    let dp=Array(strs.length+1).fill(null).map(d=>Object.assign({},possibleCases))
-    // dp[i][[k,l]] is the max number of items i can take while having used [k,l] 0,1s
-    //basecase
-    Object.keys(dp[0]).forEach(d=>dp[0][d]=0)
-    for (let i = 1; i < dp.length; i++) {
-           let [mm,nn]=strs[i-1]
-           for (const dora of Object.keys(dp[i])) {
-                let [x,y]=dora.split(',')
-           
-               
-           }
-           console.log(dp[i])
-    }   
-    return Object.values(dp[dp.length-1]).reduce((acc,curr)=>{
-       if(curr===Infinity)return acc
-       return Math.max(curr,acc) 
-    },0)
-};
 
 
 //trying sort of a hash, still knapsack
 var findMaxForm = function(strs, m, n) {
+    // map every item to its corresponding number of ones and zeros [#0s,#1s]
     for (const i in strs) {
         let container=[0,0]
         for (const bit of strs[i]) {
             container[Number(bit)]++
         }
-        strs[i]=[...container]
+        strs[i]=[...container] 
     }
-    
-  
+    let hashback=(x,y)=>(x)*(n+1)+y
+
+    // knapsack dp
     let dp=Array(strs.length+1).fill(null).map(d=>Array((m+1)*(n+1)).fill(0))
     // dp[i][[k,l]] is the max number of items i can take while having used [k,l] 0,1s    
     for (let i = 1; i < dp.length; i++) {
            let [mm,nn]=strs[i-1]
-           
+
+           //i map every j to a [x,y] pair (of all the possible pairs of 0s and 1s ) 
+           // I can end up with
            for (let j = 0,x=0,y=0; j < dp[i].length; j++) {
-               dp[i][j]=dp[i-1][j]
-               //hash j==>x,y
-               //let [x,y]=[Math.floor(j/n)-1, j-(Math.floor(j/n)-1)*n]
+                dp[i][j]=dp[i-1][j]
+
+                //these help me with the mapping
                 if(y==n+1){
                     y=0
                     x++
                 }
-               // x,y==>j  
-               // j= (1+x)*y
-               let hashback=(x,y)=>(x)*(n+1)+y
 
                if(x>=mm&&y>=nn){
                     dp[i][j]=Math.max(dp[i-1][j],1+dp[i-1][hashback(x-mm,y-nn)])
                 }
-                y++
+                y++// increment y for the next round
            }
 
       
