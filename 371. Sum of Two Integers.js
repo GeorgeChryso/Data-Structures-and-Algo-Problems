bec817ff390e79f2ab99c2d4496aac397bb41372
GeorgeChryso <lsgechrys@gmail.com>
2020-02-05 12:14:25 +0200

    bit manipulation solution added

--- 371. Sum of Two Integers.js
+++ 371. Sum of Two Integers.js
@@ -1,6 +1,8 @@
 // Calculate the sum of two integers a and b, but you are not allowed to use the operator + and -.
 
-//wrong 
+//bit manipulation 
 var getSum = function(a, b) {
-    return a+b
+    return b==0?a:getSum(a^b,(a&b)<<1)
 };
+
+
