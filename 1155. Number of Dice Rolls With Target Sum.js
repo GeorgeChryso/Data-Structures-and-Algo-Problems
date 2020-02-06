27d17b49c214c4e6a5c74da0ee5c37cf2aa3cd0a
GeorgeChryso <lsgechrys@gmail.com>
2020-02-06 19:27:36 +0200

    new dp problem

---
+++ 1155. Number of Dice Rolls With Target Sum.js
@@ -0,0 +1,8 @@
+// You have d dice, and each die has f faces numbered 1, 2, ..., f.
+
+// Return the number of possible ways (out of fd total ways) modulo 10^9 + 7 to roll the dice so the sum of the face up numbers equals target.
+
+
+var numRollsToTarget = function(d, f, target) {
+    
+};
