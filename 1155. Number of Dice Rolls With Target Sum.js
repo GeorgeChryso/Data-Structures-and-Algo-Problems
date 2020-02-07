4b26b9aa7542387a7651dc70bd604def022d8e36
GeorgeChryso <lsgechrys@gmail.com>
2020-02-07 20:39:48 +0200

    knapsack commenting

--- 1155. Number of Dice Rolls With Target Sum.js
+++ 1155. Number of Dice Rolls With Target Sum.js
@@ -2,26 +2,23 @@
 
 // Return the number of possible ways (out of fd total ways) modulo 10^9 + 7 to roll the dice so the sum of the face up numbers equals target.
 
-//knapsack
+//knapsack essentially choosing extra dices to be used as toools. Note however that every dice has to be used to produce the final outcome. 
 var numRollsToTarget = function(dices, faces, target) {
-    if(dices===30&&faces===30&&target==500)return 222616187
-
     let dp=Array(dices+1).fill(null).map(d=>Array(target+1).fill(0))
-
-    dp[0][0]=1// base case
+    // dp[i][j] means the number of ways to reach sum j by using the first i dice
+    dp[0][0]=1// base case, there is one way of getting a sum of 0 using 0 dice
     
-    for (let i = 1; i < dp.length; i++) {
-        for (let j = 0; j < dp[0].length; j++) {
-            for (let face = 1; face <=faces; face++) {
+    for (let i = 1; i < dp.length; i++) {       //for the first i dice
+        for (let j = 0; j < dp[0].length; j++) { // for everpossible sum
+            for (let face = 1; face <=faces; face++) { // for every possible dice roll
                     if(j>=face){
-                        dp[i][j]+=dp[i-1][j-face]
-                        dp[i][j]%(Math.pow(10,9)+7)              
+                        dp[i][j]+=dp[i-1][j-face]  // The sum j can only come by adding the face value to the result of the previous row( number of dice used)
+                        dp[i][j]%=(Math.pow(10,9)+7)
                     }   
             }            
         }
     
     }
-
 
     return dp[dices][target]
     
