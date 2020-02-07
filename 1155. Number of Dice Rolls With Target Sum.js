41a489efaad59ebb3b4ff9cf105edcc797ab3f8a
GeorgeChryso <lsgechrys@gmail.com>
2020-02-07 13:53:33 +0200

    knapsack solution

--- 1155. Number of Dice Rolls With Target Sum.js
+++ 1155. Number of Dice Rolls With Target Sum.js
@@ -2,7 +2,36 @@
 
 // Return the number of possible ways (out of fd total ways) modulo 10^9 + 7 to roll the dice so the sum of the face up numbers equals target.
 
+//knapsack
+var numRollsToTarget = function(dices, faces, target) {
 
-var numRollsToTarget = function(d, f, target) {
+    let result=0
+
+    let dp=Array(dices+1).fill(null).map(d=>Array(target+1).fill(0))
+
+    dp[0][0]=1// base case
+    
+    for (let i = 1; i < dp.length; i++) {
+        for (let j = 0; j < dp[0].length; j++) {
+            for (let face = 1; face <=faces; face++) {
+                    if(j>=face)dp[i][j]+=dp[i-1][j-face]                
+            }            
+        }
+    
+    }
+    result=dp[dices][target]
+
+    return result%(Math.pow(10,9)+7)
     
 };
+
+
+[
+    [1,6,3,1],
+    [2,6,7,6],
+    [2,5,10,1],
+    [1,2,3,0],
+    [30,30,500,222616187]
+].forEach(
+    ([a,b,c,result])=>console.log(numRollsToTarget(a,b,c)===result)
+)
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,18 +1,1 @@
-var climbStairs = function(n) {
-
-    //3 because its either 0, 1 or 2 
-    let dp=Array(3).fill(null).map(d=>Array(n+1).fill(0))
-
-    // dp[i][j] is how many distinct ways Can i reach cell j using i steps
-
-    //base case 
-    dp[0][0]=1
-        for (let j = 0; j < dp[i].length; j++) {
-            for (let i = 0; i < 3; i++) {
-                if(j>=i)dp[i][j]+=dp[i][j-i]
-            }
-        }        
-    
-    return dp[0][n]+dp[1][n]+dp[2][n]
-
-};
+Math.pow(10,9)+7
