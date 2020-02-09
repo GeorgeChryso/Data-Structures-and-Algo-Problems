2e7c2f5862d8ec6c0e11723f5be72b329a7cab49
GeorgeChryso <lsgechrys@gmail.com>
2020-02-09 20:51:54 +0200

    optimized dp

--- 808. Soup Servings.js
+++ 808. Soup Servings.js
@@ -14,38 +14,38 @@
 
 var soupServings = function(N) {
     if(N>=5551 )return 1 //costraint
-    let operations=[[100,0],[75,25],[50,50],[25,75]]
+    if (N==0)return 0.5 //
 
-    let dp=Array(N+101).fill(null).map(d=>Array(N+101).fill(0))
+    
+    let operations=[[100,0],[75,25],[50,50],[25,75]]
+    //If the remaining volume of soup is not enough to complete the operation, we will serve as much as we can
+    let dp=Array(N+100+1).fill(null).map(d=>Array(N+75+1).fill(0))
+    //dp[i][j] is the probability of getting i-100 ml of A and j-75 ml of B 
+
 
     //basecase 
-    dp[N+100][N+100]=1// the probability of getting Nml A Nml B 
+    dp[N+100][N+75]=1// the probability of getting Nml A Nml B 
     let pA=0
     for (let i = N+100; i>=0; i--) {
-        for (let j = N+100; j >=0; j--) {
-                if(i==N+100&&j==N+100)continue
-
-                // if(
-                //     !operations.every(
-                //         ([a,b])=>(i+a<=N+100&&j+b<=N+100)?(i+a>100&&j+a>100):true
-                //     )
-                // ){
-                //     dp[i][j]=0
-                //     continue
-                // }
-
+        for (let j = N+75; j >=0; j--) {
+                if(i==N+100&&j==N+75)continue
                 operations.forEach(
                     ([a,b])=>{
 
                         
-                        dp[i][j]+= (i+a<=N+100&&j+b<=N+100)?
-                        (i+a>100&&j+a>100)?dp[i+a][j+b]/4:0:0
+                        dp[i][j]+= 
+                            (i+a<=N+100&&j+b<=N+75)? //if a valid element
+                                (i+a>100&&j+b>75)?  //if it can come from any state where anything has ran out
+                                    dp[i+a][j+b]/4
+                                    :0  //we stop once something runs out
+                            :0
                     }
-                )                
-                if(i<=100&&j<=100)pA+=dp[i][j]/2
-                else if(i<=100)pA+=dp[i][j]
+                )             
 
-            
+                //A<=0 && B<=0 
+                if(i<=100&&j<=75)pA+=dp[i][j]/2
+                // A<=0 && B>0
+                else if(i<=100&&j>75)pA+=dp[i][j]
         }            
     }
 
@@ -56,7 +56,7 @@ var soupServings = function(N) {
 
 
 console.log(soupServings(
-   
+   //0//
    // 50  //.625
    100 //0.71875
 ))
