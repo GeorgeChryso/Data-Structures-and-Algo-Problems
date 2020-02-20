a27e1d23538d2878d17f52b33e04817f1ee2fc8d
GeorgeChryso <lsgechrys@gmail.com>
2020-02-20 23:23:49 +0200

    wrong answer

--- 897. Increasing Order Search Tree.js
+++ 897. Increasing Order Search Tree.js
@@ -1,18 +1,26 @@
 // Given a binary search tree, rearrange the tree in in-order so that the leftmost node in the tree is now the root of the tree, and every node has no left child and only 1 right child.
 
 
-
+//dfs 
 var increasingBST = function(root) {
     
-    let dfs=(node)=>{
-        if(this.left)return dfs(this.left)
-        if(this.right)return dfs(this.right)
-        return this.val
-    }
+  
     function TreeNode(val) {
-           this.val = val;
+        this.val = val;
         this.left = this.right = null;
     }
-    
-    let root=new Treenode()
+
+    let rootz=new TreeNode(0)
+
+    let result=rootz
+    let dfs=(node)=>{
+
+        if(node.left) dfs(node.left)
+        if(node.right) dfs(node.right)
+        rootz.right=new TreeNode(node.val)
+        rootz=rootz.right
+    }
+
+    dfs(root)
+    return result.right
 };
