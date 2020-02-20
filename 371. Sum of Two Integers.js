0e593547aaca1ae773152d339348cd990492264a
GeorgeChryso <lsgechrys@gmail.com>
2020-02-20 19:26:09 +0200

    commenting

--- 371. Sum of Two Integers.js
+++ 371. Sum of Two Integers.js
@@ -16,13 +16,17 @@ var getSum=(a,b)=>{
 
 //sums two positive integers
 var getSum=(a,b)=>{
-    let temp=a&b
     let result=0
     let carry=0
-    let i=1
+    let i=1 // this will be the potential element I need to OR my result with or not, and will left shift 31 times for each potential bit that needs to be set or not
     while(i!=Math.pow(2,30)){
-        result|= (((a&1)^(b&1))^carry)?i:0
+        //if the xor of all the values is 1 , that means that my bit needs to be set
+        result|= ((a&1)^(b&1)^carry)?i:0
 
+
+        //there are two cases where carry needs to be one,
+        // the first is both of the first bits of a and b being one
+        // and the second is having 1 of them (or two) as one but carrying one bit from the previous loop
         if((carry&&((a&1)||(b&1)) )||((a&1)&(b&1)) ){
             carry=1
         }
@@ -30,9 +34,10 @@ var getSum=(a,b)=>{
             carry=0
         }
 
-        console.log(result.toString(2),carry)
+        // rightshifting a,b so i can check their first bit next round
         a>>>=1
         b>>>=1
+        // left shifting i so to consider the next bit to the left for the next round
         i<<=1
     }
     return result
