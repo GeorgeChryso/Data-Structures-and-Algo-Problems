f2f8e7a51e8c82b5a775cb41f0d40eee83af03f9
GeorgeChryso <lsgechrys@gmail.com>
2020-02-20 18:46:38 +0200

    new solution only for positive integers

--- 371. Sum of Two Integers.js
+++ 371. Sum of Two Integers.js
@@ -12,3 +12,31 @@ var getSum=(a,b)=>{
     if(b===0)return a
     return getSum(a^b,(a&b)<<1)
 }
+
+
+//sums two positive integers
+var getSum=(a,b)=>{
+    let temp=a&b
+    let result=0
+    let carry=0
+    let i=1
+    while(i!=Math.pow(2,30)){
+        result|= (((a&1)^(b&1))^carry)?i:0
+
+        if((carry&&((a&1)||(b&1)) )||((a&1)&(b&1)) ){
+            carry=1
+        }
+        else{
+            carry=0
+        }
+
+        console.log(result.toString(2),carry)
+        a>>>=1
+        b>>>=1
+        i<<=1
+    }
+    return result
+}
+
+console.log(getSum(20
+    ,30))
