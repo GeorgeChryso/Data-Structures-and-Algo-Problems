2760b5924f01e791dbdaadbd6a7b81dd3ec04a3f
GeorgeChryso <lsgechrys@gmail.com>
2020-02-24 13:30:35 +0200

    commenting

--- 1262. Greatest Sum Divisible by Three.js
+++ 1262. Greatest Sum Divisible by Three.js
@@ -128,28 +128,49 @@ var maxSumDivThree = (A) => {
     for (let num of A) {
         let pre = [...cur]; // implicitly set current bucket to max of itself and previous bucket
         for (let j=0; j < 3; ++j) {
-            let sum = num + pre[j];// max sum i can achieve using the curr element
+            let sum = num + cur[j];// max sum i can achieve using the curr element and curr bucket
             let k = sum % 3; 
-            cur[k] = Math.max(cur[k], sum);
+            pre[k] = Math.max(pre[k], sum);
         }
+        cur=pre
         console.log(cur)
+
+    }
+    return cur[0];
+};
+console.log(maxSumDivThree(
+    [3,1,5]
+))
+//generalization to Greatest sum divisible by K
+var maxSumDivK = (A,K) => {
+    //cur[i] is the highest sum i can achieve such that sum%K=i
+    let cur = Array(A.length).fill(null).map(d=>0)
+
+    //Ok , so, my end target is cur[0], after considering all the elements. 
+    // But how does my array A affect cur[0]?
+    // A)Well I can incremet curr[0] automatically if my Array's element is divisible by K
+    // because if C%K==0, and Q%K==0 =>(Q+C)%K==0
+    // B) But, I can also increase curr[0] by another sum that isnt equal to any of my Array's elements, but comes from their potential and selective sum.
+    //  for example, if my K=3 and my Array=[3,1,5]
+    // when i Examine 3, my curr=[3,0,0]
+    // when I examine 1, my curr=[3,4=3+1,0]
+    // but when I examine 5, my curr becomes [9,4,8]. Why?
+    // because cur[0] was increased to 9=4+5, a sum which came from adding 5 to sum[1]
+    
+    //!!! So the second max comes from the current element I'm examining+one other element preexisting in the array. Why preexisting? Because If it's possible it'll be there as it must have %3 between 0,1 and 2
+    
+    for (let num of A) {
+        let pre = [...cur]; // implicitly set current bucket to max of itself and previous bucket
+        for (let j=0; j < K; ++j) {
+            let sum = num + cur[j];// max sum i can achieve using the curr element and curr bucket
+            let k = sum % K; 
+            pre[k] = Math.max(pre[k], sum);
+        }
+        cur=pre
     }
     return cur[0];
 };
 
-// var maxSumDivThree = (A) => {
-//     //cur[i] is the highest sum i can achieve such that sum%3=i
-//     let item=0
-
-
-//     for (let num of A) {
-//         let pre =item // implicitly set current bucket to max of itself and previous bucket
-//         let sum = num + pre;// max sum i can achieve using the curr element
-//         let k = sum % 3; 
-//         if(k==0)item = Math.max(item, sum);
-//     }
-//     return item
-// };
 console.log(
     maxSumDivThree(
       [3,6,5,1,8]//18
