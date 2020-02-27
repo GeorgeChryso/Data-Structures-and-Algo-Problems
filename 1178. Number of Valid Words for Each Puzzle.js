b880235867844dbb86a867b8271ffc754672e988
GeorgeChryso <lsgechrys@gmail.com>
2020-02-27 02:21:23 +0200

    optimized solution using a subset formula;

--- 1178. Number of Valid Words for Each Puzzle.js
+++ 1178. Number of Valid Words for Each Puzzle.js
@@ -85,8 +85,6 @@ var findNumOfValidWords = function(words, puzzles) {
 var findNumOfValidWords = function(words, puzzles) {
    
     //dictionaries for repeated words
-    let dict={}
-    let puzzledict={}
     let letters2bit=word=>{
         let final=0
         for (const letter of word) {
@@ -94,33 +92,80 @@ var findNumOfValidWords = function(words, puzzles) {
         }
         return final
     }
-
-    words=words.map(d=>{
-        if(dict[d]!==undefined)return dict[d]
-        dict[d]=letters2bit(d)
-        return dict[d]
+    let dict={}
+    words.forEach(d=>{
+        let bitrep=letters2bit(d)
+        dict[bitrep]=(dict[bitrep]||0)+1
     })
-    //different bit representations of each word
-    let diffwords=[...Object.values(dict)]
+    
+    puzzles.forEach((paz,i)=>{
+        let firstlettermask=1<<(paz.charCodeAt(0)-97)
+        let puzzle=letters2bit(paz)
+        let counter=0
+        //this part is what's causing the TLE
+        // instead of brute forcing the words,
+        // I can think smarter and consider instead every possible subset
+        // of my puzzle bitwise representation 
+        // for (var word in dict) {
+        //     word=Number(word)
+        //     if((puzzle&word)===word&&((firstlettermask&word)!==0)){
+        //         counter+=(dict[word])
+        //     }
+        // }
 
 
-    let count=0
-    for (let i = 0; i < puzzles.length &&count<=128; i++) {
-        if(puzzledict[puzzles[i]]!==undefined){
-            puzzledict[puzzles[i]]=[puzzles[i],letters2bit(puzzles[i]),1<<(puzzles[i].charCodeAt(0)-97)]
-            count++
-        }     
+        //instead, Iterate over ONLY the subsets of my number
+        let mask=puzzle
+        while(mask!=0){
+            if((firstlettermask&mask)!==0){
+                counter+=dict[mask]||0
+            }
+            mask=(mask-1)&puzzle //finds all the subsets of my mask
+        }
+
+        puzzles[i]=counter
+      })
+
+    return puzzles
+};
+
+console.log(findNumOfValidWords(
+    ["aaaa","asas","able","ability","actt","actor","access"],
+["aboveyz","abrodyz","abslute","absoryz","actresz","gaswxyz"]
+))
+var findNumOfValidWords = function(words, puzzles) {
+    const toMask = function(str) {
+        let res = 0;
+        for (const ch of str) {
+            res |= 1 << (ch.charCodeAt(0) - 'a'.charCodeAt(0));
+        }
+        return res;
+    }
+    const counts = new Map();
+    for (const word of words) {
+        const mask = toMask(word);
+        counts.set(mask, counts.has(mask) ? counts.get(mask) + 1 : 1);
     }
 
-
-    let resultdict={}
-
- 
-    return puzzles.map((paz)=>{
-        if(resultdict[paz]!==undefined)return resultdict[paz]
-        let [original,puzzle,firstlettermask]=puzzledict[paz]
-        resultdict[paz]=diffwords.reduce((acc,word)=>acc+Number(((puzzle&word)==word )&& ((firstlettermask&word)!==0)),0)
-        return resultdict[paz]
-     }
-    )
+    const res = [];
+    for (const puzzle of puzzles) {
+        const mask = toMask(puzzle);
+        const first = 1 << (puzzle.charCodeAt(0) - 'a'.charCodeAt(0));
+        let cnt = 0, sub = mask;
+        //essentially brute forces every number less than my current
+        //which can be a subset of my puzzle bitwise representation, 
+        // saving a lot of time. could be saved
+        while (sub !== 0) {
+            if ((sub & first) && counts.has(sub)) {
+                cnt += counts.get(sub);
+            }
+            sub = (sub - 1) & mask; //fookin genius or what
+        }
+        res.push(cnt);
+    }
+    return res;
 };
+
+
+
+
