f068891fea9a8967c8e30ab1434cf8f24f235bae
GeorgeChryso <lsgechrys@gmail.com>
2020-03-04 02:46:07 +0200

    new solution

---
+++ 218. The Skyline Problem.js
@@ -0,0 +1,60 @@
+// A city's skyline is the outer contour of the silhouette formed by all the buildings in that city when viewed from a distance. Now suppose you are given the locations and height of all the buildings as shown on a cityscape photo (Figure A), write a program to output the skyline formed by these buildings collectively (Figure B).
+// The geometric information of each building is represented by a triplet of integers [Li, Ri, Hi], where Li and Ri are the x coordinates of the left and right edge of the ith building, respectively, and Hi is its height. It is guaranteed that 0 ≤ Li, Ri ≤ INT_MAX, 0 < Hi ≤ INT_MAX, and Ri - Li > 0. You may assume all buildings are perfect rectangles grounded on an absolutely flat surface at height 0.
+
+
+// The output is a list of "key points" (red dots in Figure B) in the format of [ [x1,y1], [x2, y2], [x3, y3], ... ] that uniquely defines a skyline. A key point is the left endpoint of a horizontal line segment. Note that the last key point, where the rightmost building ends, is merely used to mark the termination of the skyline, and always has zero height. Also, the ground in between any two adjacent buildings should be considered part of the skyline contour.
+
+
+// The number of buildings in any input list is guaranteed to be in the range [0, 10000].
+// The input list is already sorted in ascending order by the left x position Li.
+// The output list must be sorted by the x position.
+// There must be no consecutive horizontal lines of equal height in the output skyline. For instance, [...[2 3], [4 5], [7 5], [11 5], [12 7]...] is not acceptable; the three lines of height 5 should be merged into one in the final output as such: [...[2 3], [4 5], [12 7], ...]
+
+
+/* B [Li,Ri,Hi]
+*/
+// O(n^2) runtime O(n) space
+var getSkyline = function(B) {
+    let mapped=[]
+
+    //seperate its block to its top left corner and bottom right corner coordinates
+    B.forEach(
+        ([L,R,H])=>{
+            mapped.push([L,H],[R,0])
+        }
+    )
+    
+    //if there is an overlapping block with bigger height, update the y of each of my new [x,y] elements 
+    mapped=mapped.map(
+        ([x,y])=>{
+           let newy=0
+           for (const [L,R,H] of B) {
+               
+               if(L<=x&&x<R){
+                    newy=Math.max(H,newy)
+               }
+               if(L>x)break
+           }
+           
+           return [x,newy]
+        }
+    )
+    
+    
+    //sort the array so i can remove the redundant elements
+    mapped.sort((a,b)=>{
+        // if my elements are overlapping on x, always choose the bigger y
+        if(a[0]==b[0]){
+            return b[1]-a[1]
+        }
+        return a[0]-b[0]
+    })
+    //remove the reduntant elements which are the ones with equal height with their previous element
+    return mapped.filter(([x,y],i)=>i>=1?y!==mapped[i-1][1]:true)
+};
+
+console.log(
+    getSkyline(
+        [[2,9,10],[3,7,15],[5,12,12],[15,20,10],[19,24,8]]
+            )
+)
