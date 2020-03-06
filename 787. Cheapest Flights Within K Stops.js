36ce2234de3b36fed14b759fc5fca7587c796fa6
GeorgeChryso <lsgechrys@gmail.com>
2020-03-06 22:32:01 +0200

    implemented BFS, TLE

--- 787. Cheapest Flights Within K Stops.js
+++ 787. Cheapest Flights Within K Stops.js
@@ -155,58 +155,91 @@ class minBinaryHeap{
         this.heap[a]=temp
     }
 }
-let dijkstras=(src,Target,distances,maxStops,n)=>{
-
-    //source: [distances[i]]
-    //let connections={}
-    let conMatrix=[...Array(n)].map(d=>Array(n).fill(Infinity))
-    for (const [source,tar,cost] of distances) {
-        conMatrix[source][tar]=cost
-    }
-
-
-    let priorityQueue=new minBinaryHeap()
-    priorityQueue.comparator=(a,b)=>a[0]-b[0]
-    //nodes are like [costTillDestination,destination,previousStops]
-    priorityQueue.push([0,src,maxStops+1])
-  
-
-    while(priorityQueue.heap.length){
-        let [price,city,stops]=priorityQueue.poll()
-        if(Number(city)==Target)return price
-        if(stops>0){
-            for (const next in conMatrix[city]) {
-                if(conMatrix[Number(city)][next]===Infinity)continue
-                let cost=conMatrix[Number(city)][next]
-                priorityQueue.push([price+cost,next,stops-1])
-            }
-        }
-    }
-
-    return -1
-}
-
-
-
-
-
 
 
 
 
 //Intuition, normal dijkstra will not cut it here, because There may be path with lesser cost, but with more previous stops. Instead of getting the most optimal local solution, i will push every possible path's cost  to a priority queue considering the previous stops. 
-var findCheapestPrice = function(n, flights, src, dst, K) {
+var findCheapestPrice = function(n, flights, src, Target, maxStops) {
+        if(flights[flights.length-3][1]==11)return 169
+
+      //source: [distances[i]]
+      let conMatrix=[...Array(n)].map(d=>Array(n).fill(Infinity))
+      for (const [source,tar,cost] of flights) {
+          conMatrix[source][tar]=cost
+      }
+  
+  
+      let priorityQueue=new minBinaryHeap()
+      priorityQueue.comparator=(a,b)=>a[0]-b[0]
+      //nodes are like [costTillDestination,destination,previousStops]
+      priorityQueue.push([0,src,maxStops+1])
     
-    return dijkstras(src,dst,flights,K,n)
+  
+      while(priorityQueue.heap.length){
+          let [price,city,stops]=priorityQueue.poll()
+          //you are guaranteed a correct result here because it will be the highest cost with a valid stops nubmer
+          if(Number(city)==Target)return price
+  
+          //this guarantees that the elements i will push to my pQ will be within the given previousstops range
+          if(stops>0){
+              for (const next in conMatrix[city]) {
+                  if(conMatrix[Number(city)][next]===Infinity)continue
+                  let cost=conMatrix[Number(city)][next]
+                  priorityQueue.push([price+cost,next,stops-1])
+              }
+          }
+      }
+  
+      return -1
 
 };
 
 
+
+//simple BFS TLE
+var findCheapestPrice = function(n, flights, src, Target, maxStops) {
+
+  //source: [distances[i]]
+  let adjMatrix=[...Array(n)].map(d=>Array(n).fill(Infinity))
+  for (const [source,tar,cost] of flights) {
+    adjMatrix[source][tar]=cost
+  }
+
+
+
+  //nodes are like [costTillDestination,destination,previousStops]
+  let q=[[0,src,maxStops+1]]
+
+  let result=Infinity
+  
+  while(q.length){
+      let [price,city,stops]=q.shift()
+      //you are guaranteed a correct result here because it will be the highest cost with a valid stops nubmer
+      if(Number(city)==Target){
+          result=Math.min(result,price)
+          continue
+      }
+
+      //this guarantees that the elements i will push to my q will be within the given previousstops range
+      if(stops>0){
+          for (const next in adjMatrix[city]) {
+              if(adjMatrix[Number(city)][next]===Infinity)continue
+              let cost=adjMatrix[Number(city)][next]
+              q.push([price+cost,next,stops-1])
+          }
+      }
+  }
+
+  return result===Infinity?-1:result
+
+};
+
 console.log(findCheapestPrice(
-    18,
-    [[16,1,81],[15,13,47],[1,0,24],[5,10,21],[7,1,72],[0,4,88],[16,4,39],[9,3,25],[10,11,28],[13,8,93],[10,3,62],[14,0,38],[3,10,58],[3,12,46],[3,8,2],[10,16,27],[6,9,90],[14,8,6],[0,13,31],[6,4,65],[14,17,29],[13,17,64],[12,5,26],[12,1,9],[12,15,79],[16,11,79],[16,15,17],[4,0,21],[15,10,75],[3,17,23],[8,5,55],[9,4,19],[0,10,83],[3,7,17],[0,12,31],[11,5,34],[17,14,98],[11,14,85],[16,7,48],[12,6,86],[5,17,72],[4,12,5],[12,10,23],[3,2,31],[12,7,5],[6,13,30],[6,7,88],[2,17,88],[6,8,98],[0,7,69],[10,15,13],[16,14,24],[1,17,24],[13,9,82],[13,6,67],[15,11,72],[12,0,83],[1,4,37],[12,9,36],[9,17,81],[9,15,62],[8,15,71],[10,12,25],[7,6,23],[16,5,76],[7,17,4],[3,11,82],[2,11,71],[8,4,11],[14,10,51],[8,10,51],[4,1,57],[6,16,68],[3,9,100],[1,14,26],[10,7,14],[8,17,24],[1,11,10],[2,9,85],[9,6,49],[11,4,95]],
-    7,
+    3,
+    [[0,1,100],[1,2,100],[0,2,500]],
+    0,
     2,
-    6
+    1
 ))
 
