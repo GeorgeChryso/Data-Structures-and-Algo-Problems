76bdf108d82adbc7d99cf88d9f56e0ea410602f7
GeorgeChryso <lsgechrys@gmail.com>
2020-03-07 20:54:05 +0200

    mv

--- Whiteboard Material/1371. Find the Longest Substring Containing Vowels in Even Counts.js
+++ Whiteboard Material/1371. Find the Longest Substring Containing Vowels in Even Counts.js
@@ -34,19 +34,22 @@ var findTheLongestSubstring = function(s) {
 };
 
 
-//Bitmask solution
+//Bitmask solution- PREFIX XOR
 var findTheLongestSubstring=s=>{
-    let seen={}
+    let seen={} // key: bitmask, value: the first index at which it occurs
     let cur=0
-    let res=0
-
+    let res=0 
+    //basecase
+    seen[0]=-1 //because if I come across 0 at any time during my traversal, I will need to consider that length
     for (let i = 0; i < s.length; i++) {
         cur^=1<<('aeiou'.indexOf(s[i])+1)>>1
         if(seen[cur]===undefined)seen[cur]=i
-        res=Math.max(res,i-seen[cur])
+        //the length of the string, between the first index at which my cur occured and the last
+        // needs to be considered as a result, because that means that the inbetween string has a XOR of 0
+        // which makes it of the required type
+        res=Math.max(res,i-seen[cur]) 
     }
 
 
     return res
-
 }
