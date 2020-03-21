bbf31cb7efaf07df826e08c4fc90552b66cb1428
GeorgeChryso <lsgechrys@gmail.com>
2020-03-21 22:57:40 +0200

    Deterministic Select

--- 1387. Sort Integers by The Power Value.js
+++ 1387. Sort Integers by The Power Value.js
@@ -138,10 +138,82 @@ class MaxHeap {
     }
   }
   
+
+
+  // Deterministic Select
+  var getKth = function(lo, hi, k) {
+    if(lo===hi)return lo
+
+    
+    let steps=(x)=>{
+        let result=0
+        while(x!=1){
+            x=x%2?3*x+1:x/2
+            result++
+        }
+        return result
+    }
+
+    let pq= []
+
+    for (let i = lo; i < hi+1; i++) {
+        pq.push([i,steps(i)])        
+    }
+    console.log(pq)
+    let comparator=(a,b)=>a[1]==b[1]?a[0]-b[0]:a[1]-b[1]
+
+    console.log(pq.sort(comparator))
+
+    return Dselect(pq,pq.length-k)[0]
+};
+
+let Dselect=(A,k)=>{
+    let comparator=(a,b)=>a[1]==b[1]?a[0]-b[0]:a[1]-b[1]
+
+
+    if(A.length<10){
+        A.sort(comparator)
+        return A[k]
+    }
+  
+    let subsets=[]
+    let group=[]
+    for (var i = 0; i < A.length; i++) {
+        if(group.length==5){ 
+            subsets.push(group)
+            group=[]
+        }
+        group.push(A[i])
+    }
+    subsets[subsets.length-1]=subsets[subsets.length-1].concat(group)
+    
+    let mediansOfSubsets=[]
+    for (const subset of subsets) {
+        mediansOfSubsets.push(Dselect(subset,3)) //guaranteed to hit the first if Clause
+    }
+  
+    let M= Dselect(mediansOfSubsets,Math.ceil(mediansOfSubsets.length/2))
+  
+    
+    //partition around Pivot M
+    let L1=[],L2=[],L3=[]
+    let pivot=M 
+    for (let j = 0; j < A.length; j++) {
+        if(comparator(A[j],pivot)<0) L1.push(A[j])
+        else if( comparator(A[j],pivot)===0)L2.push(A[j])
+        else L3.push(A[j])
+    }
+  
+    if (k <=L1.length)return Dselect(L1,k)
+    else if (k> L1.length+L2.length)return Dselect(L3,k-L1.length-L2.length)
+    else return L2[0] //k===L1.length+1
+  }
+  
   
 console.log(
     getKth(
-        12,15,2
-      //  7,11,4
+      //  12,15,2
+       // 7,11,4
+      7,11,4
     )
 )
