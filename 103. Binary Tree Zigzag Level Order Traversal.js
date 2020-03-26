997896f7fd6dc1f67b88e4f25f99d35c371d7229
GeorgeChryso <lsgechrys@gmail.com>
2020-03-26 22:16:05 +0200

    BFS with a hack

--- 103. Binary Tree Zigzag Level Order Traversal.js
+++ 103. Binary Tree Zigzag Level Order Traversal.js
@@ -1,5 +1,31 @@
 
 
 var zigzagLevelOrder = function(root) {
-    
+    if(root===null)return []
+
+    let result=[]
+    let q=[root]
+    let flag=true
+    while(q.length){
+        let tempres=[]
+        let temp=[]
+        for (const node of q) {
+            if(node===null){
+                tempres.push(null)
+                continue
+            }
+            if(node.left){
+                temp.push(node.left)
+            }
+            if(node.right){
+                temp.push(node.right)
+            }
+            tempres.push(node.val)
+        }
+        if(flag)result.push(tempres)
+        else result.push(tempres.reverse())
+        q=temp
+        flag=!flag
+    }
+    return result
 };
