847c5e713fa4b78eece29494e0498c594a3b821b
GeorgeChryso <lsgechrys@gmail.com>
2020-04-03 04:01:03 +0300

    Floyds cycle detection algo

--- 202. Happy Number.js
+++ 202. Happy Number.js
@@ -17,3 +17,27 @@ var isHappy = function(n) {
 };
 
 console.log(isHappy(7))
+
+
+// Floyd's Cycle Detection 
+var isHappy = function(n) {
+
+    let next=x=>{
+        let sum=0
+        while(x>=1){
+            sum+=(x%10)**2
+            x=Math.floor(x/10)
+        }
+        return sum
+    }
+
+    let slow=n 
+    let fast=next(n)
+    while(slow!=fast){
+        if(slow==1||fast==1)return true
+        slow=next(slow)
+        fast=next(next(fast))
+    }
+    if(slow==1||fast==1)return true
+    return false
+};
