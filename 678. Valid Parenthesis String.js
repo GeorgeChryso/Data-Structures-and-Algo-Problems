ac1da5294d7318ac967cb65c13ad9d44279d9c49
GeorgeChryso <lsgechrys@gmail.com>
2020-04-16 11:49:06 +0300

    Lee approach

--- 678. Valid Parenthesis String.js
+++ 678. Valid Parenthesis String.js
@@ -12,44 +12,24 @@
 
 
 var checkValidString = function(s) {
-    if(s.length==1&&s!='*')return false
-    let possibilities=[]
-
-    let dfs=(idx,arr)=>{
-      
-        while(s[idx]!=="*"&&idx<s.length)idx++
-        if(idx>=s.length){
-            possibilities.push(arr)
-            return
+    let cmin=0,cmax=0
+    for (let i = 0; i < s.length; ++i) {
+        let c = s.charAt(i);
+        if (c == '(') {
+            cmax++;
+            cmin++;
+        } else if (c == ')') {
+            cmax--;
+            cmin = Math.max(cmin - 1, 0);
+        } else {
+            cmax++;
+            cmin = Math.max(cmin - 1, 0);
         }
-
-        
-        let l=[...arr]
-        l[idx]='('
-        let m=[...arr]
-        m[idx]=''
-        let r=[...arr]
-        r[idx]=')'
-
-        dfs(idx+1,l)
-        dfs(idx+1,m)
-        dfs(idx+1,r)
+        if (cmax < 0) return false;
     }
+    return cmin == 0;
+}
 
-    dfs(0,s.split(''))
-
-    let val=arr=>{
-        let stack=[]
-        for (const ss of arr) {
-            if(ss=='(')stack.push(ss)
-            else if(ss==')'&&stack.pop()!='(')return false
-        }
-
-        return true
-    }
-
-    return possibilities.some(d=>val(d))
-};
 
 
 console.log(checkValidString(
