a2556cbebf4b10f27896e5c8ea5cedeffa5332a2
GeorgeChryso <lsgechrys@gmail.com>
2020-04-30 11:19:44 +0300

    challenge30;

---
+++   Check If a String Is a Valid Sequence from Root to Leaves Path in a Binary Tree.js
@@ -0,0 +1,22 @@
+// Check If a String Is a Valid Sequence from Root to Leaves Path in a Binary Tree
+
+
+
+
+
+
+
+var isValidSequence = function(root, arr) {
+    
+    if(arr.length===1)return root.val==arr[0]&&root.left===null&&root.right===null
+    let dfs=(node,idx)=>{
+
+        if(node==null&&idx<arr.length)return false
+        if(idx==arr.length-1)return node.val==arr[idx]&&node.left===null&&node.right===null
+        if(idx==arr.length&&node!==null)return false
+
+        return node.val==arr[idx]&&(dfs(node.left,idx+1)||dfs(node.right,idx+1))
+    }
+
+    return dfs(root,0)
+};
