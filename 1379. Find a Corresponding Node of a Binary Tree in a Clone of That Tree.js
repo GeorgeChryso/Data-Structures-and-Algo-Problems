1a71c70befe18096f480f400dae534740b8e8733
GeorgeChryso <lsgechrys@gmail.com>
2020-05-07 20:33:53 +0300

    short n fluffly

--- 1379. Find a Corresponding Node of a Binary Tree in a Clone of That Tree.js
+++ 1379. Find a Corresponding Node of a Binary Tree in a Clone of That Tree.js
@@ -3,16 +3,24 @@
 
 // The cloned tree is a copy of the original tree.
 
-//js reference comparison with ==
+//simple dfs for unique value
 var getTargetCopy = function(original, cloned, target) {
     let result
     let dfs=node=>{
-        if(node.val==target.val){
-            result= node
-        }
-        if(node.left) dfs(node.left)
-        if(node.right) dfs(node.right)
+        if(node.val==target.val)result= node
+        else (node.left&&dfs(node.left))||(node.right&& dfs(node.right))
     }
     dfs(cloned)
+    return result
+};
+
+//dfs js reference comparison with ==
+//kewlkids
+var getTargetCopy = function(original, cloned, target) {
+    let result
+    let dfs=(node,bode)=>
+        (node==target)?result= bode:
+        (node.left&&dfs(node.left,bode.left))||(node.right&& dfs(node.right,bode.right))
+    dfs(original,cloned)
     return result
 };
