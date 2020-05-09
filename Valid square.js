3a2270028be426ce3375fc3c071fa16a5bd1e3fa
GeorgeChryso <lsgechrys@gmail.com>
2020-05-09 15:52:46 +0300

    Challenge May 9

--- Valid square.js
+++ Valid square.js
@@ -3,20 +3,26 @@
 
 // Approach 1, using a binary search on the smaller numbers of my number, determine if 
 // instead, there is a number whose power of two equals my target
-let isAsquare=X=>{
+
+let isPerfectSquare=X=>{
     if(X<0)return false
+    if(X==0||X==1)return true
 
-    let L=0
-    let R=X
+    let lo=0
+    let hi= X/2
 
-    while(L<=R){
-        mid=L+Math.floor((R-L)/2)
-        if(mid**2==X)return true
-        if(mid**2>X)R=mid-1
-        if(mid**2<X)L=mid+1
+    while(lo<=hi){
+        let mid= Math.floor(lo+(hi-lo)/2)
+        let sq=mid**2
+        if(sq==X)return true
+        if(sq<X)lo=mid+1
+        if(sq>X)hi=mid-1
     }
-
     return false
+
 }
+
+
+
 //Complexity O(logn) Time O(1) Space
 console.log(isAsquare(9000))
