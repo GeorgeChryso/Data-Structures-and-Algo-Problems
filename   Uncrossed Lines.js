a899968f8aa32d68f6e067c22f88645c9f5f429a
GeorgeChryso <lsgechrys@gmail.com>
2020-05-25 21:56:19 +0300

    minor

---   Uncrossed Lines.js
+++   Uncrossed Lines.js
@@ -21,9 +21,9 @@ var maxUncrossedLines = function(A, B) {
         
         for(let j = 1; j <= n; ++j){
             if (A[i - 1] == B[j - 1])
-            dp[i][j] = 1 + dp[i - 1][j - 1];
-       else
-            dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+            else
+                dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
         }
     }
 
