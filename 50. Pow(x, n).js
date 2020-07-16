0bf7b717d8bddb107371e5bbcefc64e135cccf95
GeorgeChryso <lsgechrys@gmail.com>
2020-07-16 13:12:49 +0300

    commenting

--- 50. Pow(x, n).js
+++ 50. Pow(x, n).js
@@ -7,9 +7,12 @@
 // effectively computing powers 1 ,2 ,4, 8, 16....
 // I CAN MAKE ANY NUMBER USING THOSE POWERS.
 
+
+// O( lgn )
 var myPow = function(x, n) {
     if(n===0)return 1
-    let result=1
+    let result=1 //THE ACCUMULATOR, THIS WILL BE MULTIPLIED WITH ALL THE X**(2**i) necessary to form X**N
+    // reminder: n= 2**a + 2**b +... for some powers of 2 .
     // if n is negative i m gonna compute the positive power
     // and return 1/result
     let q=Math.max(n,-n)
