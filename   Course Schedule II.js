8ee05389b8aea56e36c7b84d7237d83e88b23c57
GeorgeChryso <lsgechrys@gmail.com>
2020-07-18 14:02:19 +0300

    topo sorting;

---   Course Schedule II.js
+++   Course Schedule II.js
@@ -51,9 +51,60 @@ var findOrder = function(n, prerequisites) {
     return result
 };
 
+
+//topological sort dfs
+var findOrder = function(n, prerequisites) {
+    let adj=[...Array(n)].map(d=>[...Array(n)].map(q=>0)) 
+    for (const [next,prev] of prerequisites) {
+        adj[prev][next]=1
+    }
+
+    //has cycles? through naive dfs TLE O(V+E)
+    let visited=[...Array(n)].map(d=>0)
+    let finished=new Set()
+    let hasCycles=node=>{
+        if(visited[node])return true
+        if(finished.has(node))return false
+        visited[node]=true
+        finished.add(node)
+        if(adj[node])
+           for (let i = 0; i < adj[node].length; i++) {
+                if(adj[node][i]){
+                    if(hasCycles(i))return true
+                }               
+           }
+        visited[node]=false
+        return false
+    }
+
+    for (let i = 0; i < n; i++) {   
+        if(hasCycles(i))return [] 
+    }
+
+
+
+    //topo sort O(V+E)
+    visited=new Set()
+    let result=[]
+    let dfs=(node)=>{
+        if(node===undefined||visited.has(node))return
+        visited.add(node)
+        for (let i = 0;adj[node]&& i < adj[node].length; i++) {
+           if(adj[node][i]) dfs(i)            
+        }
+        result.unshift(node)
+    }
+
+    for (let i = 0; i < n; i++) {
+        dfs(i)        
+    }
+    
+    return result
+};
+
 console.log(
     findOrder(
-        2,
-        [[0,1],[1,0]]
+        2,[[1,0]]
+     //2,   [[0,1],[1,0]]
     )
 )
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,2 +1,1 @@
-
-    )
+        checked.add(node)
