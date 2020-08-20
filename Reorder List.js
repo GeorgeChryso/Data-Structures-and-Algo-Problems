a85a4ab74adbb72004bf9281786fbe04d7d071e0
GeorgeChryso <lsgechrys@gmail.com>
2020-08-21 01:48:56 +0300

    commenting

--- Reorder List.js
+++ Reorder List.js
@@ -7,21 +7,25 @@
 
 
 var reorderList = function (head) {
-    let stack = [], node = head
+    let q = [], node = head
     if (!node) return
+    //push all the nodes in a q
     while (node) {
-      stack.push(node)
+      q.push(node)
       node = node.next
     }
   
-    let len = stack.length
+    let len = q.length
     node = head
     for (let i = 0; i < len; i++) {
+      //take one from the beginning
       if (i % 2 === 0)
-        node.next = stack.shift()
+        node.next = q.shift()
+      //and one from the end
       else
-        node.next = stack.pop()
+        node.next = q.pop()
       node = node.next
     }
-    node.next = null
+    // dont forget to set the last node's next to null
+    node.next = null 
   };
