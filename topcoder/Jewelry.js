d3d1d9bae1bea1436e6b7c91d4210dd1b4171410
GeorgeChryso <lsgechrys@gmail.com>
2020-08-27 00:49:51 +0300

    Hard Dp problem 10 days

--- topcoder/Jewelry.js
+++ topcoder/Jewelry.js
@@ -28,6 +28,7 @@
 
 
 //calculates the number of ways to reach various sums (Knapsack Variant)
+// Helper function
 var numWays=(Arr)=>{
     let maxSum=Arr.reduce((acc,curr)=>acc+curr)
     let dp=[...Array(Arr.length+1)].map(d=>[...Array(maxSum+1).fill(0)])
@@ -40,11 +41,11 @@ var numWays=(Arr)=>{
             dp[i][k]+=dp[i-1][k]            
         }        
     }
-    return dp[Arr.length] //result means that u can reach sum k in result[k] ways
+    return dp 
 }
 
 //the above function can of course be reduced to O(maxSum) space
-var numWays=(Arr)=>{
+var numWaysShort=(Arr)=>{
     let maxSum=Arr.reduce((acc,curr)=>acc+curr)
     let dp=[...Array(maxSum+1)].map(d=>0)
     //dp[k]= number of ways of reaching sum k using the first i elements 
@@ -57,7 +58,6 @@ var numWays=(Arr)=>{
     }
     return dp //result means that u can reach sum k in result[k] ways
 }
-console.log(numWays([1,2,5,3,4,5]))
 
 
 // ATTENTION
@@ -96,42 +96,63 @@ let howMany=A=>{
 
     //boilerplate for combinations
     combmemo=[...Array(n+1)].map(d=>[...Array(n+1)]) //combinations n choose k 
+    for (let i = 1; i <=n; i++) {
+        combmemo[i][0]=1 //combinations n take 0 is 1 
+        combmemo[i][i]=1 //combinations n take n is  1 
+    }
+    for (let i = 1; i <=n; i++) {
+        for (let k = 1; k <i; k++) {
+            combmemo[i][k]=combmemo[i-1][k-1]+combmemo[i-1][k]            
+        }        
+    }
+ 
     var combinations=(n,k)=>{
         //base cases
         if(n===k||k===0)return 1
+        if(k==1)return n
         if(combmemo[n][k]!==undefined)return combmemo[n][k]
         combmemo[n][k]=combinations(n-1,k-1)+combinations(n-1,k)//known formula
         return combmemo[n][k]
     }
 
-    A=A.sort((a,b)=>a-b)//we want to find a split point on the sorted list to satisfy Policy 1
+    A.sort((a,b)=>a-b)//we want to find a split point on the sorted list to satisfy Policy 1
 
-    //I will create 2 matrices
+    //          I will create 2 matrices
+
+    
     // WaysBelow is an n x Sum(MaxSum) matrix
-    // where WaysBelow[s][i]= the number of ways to reach sum s using(some of)the first i elements
+    let WaysBelow=numWays(A)
+    // where WaysBelow[i][s]= the number of ways to reach sum s using(some of)the first i elements
     // WaysAbove isa  nx Sum(MaxSum)matrix 
-    // where WaysAbove[s][i]= the number of ways to reach sum s using(some of)the last i items
+    //WaysBelow.forEach(d=>console.log(d+''))
+
+
+
+    let B=[...A]
+    B.reverse()
+    let WaysAbove=numWays(B)
+    // where WaysAbove[i][s]= the number of ways to reach sum s using(some of)the last i items
+    //WaysAbove.forEach(d=>console.log(d+''))
 
 
     //then
     let result=0
 
-    //for every cut point
+    //for every cut point (cut point doesnt actually go well with what i m doing regarding DUPLICATES (read below))
+    // but rather, it is a cut point that separates different elements and not duplicates
     for (let i = 0; i <n; ) {
-
         let count=1 //count of consecutive A[i] s (duplicates)
         while(i+count<n&&A[i+count]==A[i]){
             count++
         }        
-        
         //how many A[i]'s am I (BOB)gonna use?
-        // so essentially i m making the cut 
-        for (let j = i; j < i+count; j++) {
-            let startSum=A[i]*(j-i+1)// I m using at least (j-i+1) A[i]'s
+        for (let j = 1; j <=count; j++) { // <======== DO NOT think of this as a cut , it's just the POSSIBLE number of the same elements i m allowed to pick 
+            let startSum=A[i]*j // I m using  j  A[i]'s
             // so i m gonna update the sums from this point onwards
-            for (let sum = startSum; sum <=MaxSum; sum++) 
-                result+=combinations(count,j-i+1) * WaysBelow[sum-A[i]*(j-i+1)][i]* WaysAbove[sum][n-i-j]
-            
+            for (let sum = startSum; sum <=MaxSum; sum++){
+                result+=combmemo[count][j] * WaysBelow[i][sum-(A[i]*j)] * WaysAbove[n-i-j][sum]
+            }
+            //in how many ways can I Pick these A[i]'s //include the A[i] //# of ways onwards
         }
 
         //noticethat i m incrementing my i by the count
@@ -142,8 +163,26 @@ let howMany=A=>{
 }
 //https://stackoverflow.com/questions/48059479/need-help-understanding-the-solution-for-the-jewelry-topcoder-solution/48061551#48061551
 
-// What I cannot understand: Why do duplicates mess up the result?
+// QUESTION: What I cannot understand: Why do duplicates mess up the result?
+// ANSWER: That was easy afterall, consider the array [5,5] and target =10
+// what are the results? 
+// output should be 2, because bob can take both the first 5 and the second 5, 
+// with the possible answers being [[5,5], [5,5]]
+// this is precisely why WaysBelow[sum-A[i]]*WaysAbove[sum] WOULDNT work, because 
+// waysbelow would be 1 ( the first 5) and ways Above would be 1 ( the second 5)
+// so the result would be 1*1=1 =/= 2 (THE CORRECT ANSWER)
 
+// Remedy: 
+
+// Combinatorics: 
+// Example: 
+// Say I have an array of [1,2,2,2,2,4]
+
+// I have calculated my waysAbove and WaysBelow Tables through typical knapsack
+// Now, during my typical iteration of elements, i come across the first 2
+// Let's say that Bob and Patrick want to both pick elements that add up to 2. 
+// Now that would have to be 
+// [{2},{2}] but with all the combinations of two's  possible. 
 
 
 let cases=[
@@ -156,5 +195,8 @@ let cases=[
         1000,1000,1000,1000,1000], //18252025766940
     [1,2,3,4,5],//4
     [7,7,8,9,10,11,1,2,2,3,4,5,6],//607
-    [123,217,661,678,796,964,54,111,417,526,917,923] //0
+     [123,217,661,678,796,964,54,111,417,526,917,923] //0
 ]
+
+
+cases.forEach(d=>console.log(howMany(d)))
