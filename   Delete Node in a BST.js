476f86c52528ec0a0685bdb7645a5fbd3cfd4e6a
GeorgeChryso <lsgechrys@gmail.com>
2020-09-01 21:59:49 +0300

    commenting and AC

---   Delete Node in a BST.js
+++   Delete Node in a BST.js
@@ -89,10 +89,16 @@ var deleteNode = function(root, key) {
     
     //if it has a right node substitute it with the right cousin (successor)
     if(todel.right){
-        if(todel.right.left==null)
+        if(todel.right.left==null){
+            todel.right.left=todel.left
             todelParent[direction]=todel.right
+        }
         else{
-            
+            let leftmost=todel.right
+            while(leftmost.left.left)
+                leftmost=leftmost.left
+            todel.val=leftmost.left.val
+            leftmost.left=leftmost.left.right
         }
     }
     //else just substitute it with its left element
@@ -102,3 +108,47 @@ var deleteNode = function(root, key) {
     return root
 };
 
+var deleteNode = function(root, key) {
+    // edge case 
+    if (!root) 
+        return null;
+    var node = root, parent,direction;
+    
+    while (node && node.val !== key) {
+        parent = node;
+        if (key > node.val) {
+            node = node.right;
+            direction = 'right';
+        } else {
+            node = node.left;
+            direction = 'left';
+        }
+    }
+
+    if (node === null) return root
+        
+    if (node.right) {
+        // make the right branch the new parent/root
+        // first find the left-most node on the right branch
+        let rightBranchLeftMost = node.right;
+        while (rightBranchLeftMost.left) 
+            rightBranchLeftMost = rightBranchLeftMost.left;
+        
+        // attach the old root's left to that
+        rightBranchLeftMost.left = node.left
+        
+        if (parent) 
+            parent[direction] = node.right;
+         else 
+            // When we have no parent, we're talking about root
+            root = node.right;    
+    } 
+    else {
+        if (parent) 
+            parent[direction] = node.left;
+        else 
+        // When we have no parent, we're talking about root
+            root = node.left;  
+    }   
+    return root;
+};
