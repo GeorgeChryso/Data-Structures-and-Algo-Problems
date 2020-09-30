b18cb3c90c0e582f34687d26a1200d6093ecb230
GeorgeChryso <lsgechrys@gmail.com>
2020-09-30 14:53:52 +0300

    daily

--- 41. First Missing Positive.js
+++ 41. First Missing Positive.js
@@ -1,34 +1,42 @@
-'use strict'
 
 // Given an unsorted integer array, find the smallest missing positive integer.
 
 // Example 1:
 
 // Input: [1,2,0]
-// Output: 3
 
+
+
+
+
+
+
+// constant EXTRA space, so i m gonna alter my array
+// intuition: The indexes of my array are already a memo i can utilize,
+// If i save the number corresponding to that index i m golden
+// O(n), cos i visit each cell at most twice, and Constant extra space, cos i m only using my array
 var firstMissingPositive = function(nums) {
-    let z=  Math.max(... nums.filter(
-    (d)=>d>0?d:0));
-      
-    for (var i=1; i<=z; i++){
-        
-      if  (nums.includes(i)){
-        
-      } 
-       else{
-         break;
-       
-       }
-        
-    } 
-      
-        return i
-        
-    };
+    let n=nums.length
+    // put each nums[i]= i+1, ignore the rest of the numbers (aka <=0 ||>n)
+    for (let i = 0; i < n; i++){
+        let tosort=nums[i]
+        while(tosort>=1&&tosort<=n&&nums[tosort-1]!==tosort){
+            let temp=tosort
+            tosort=nums[tosort-1]
+            nums[temp-1]=temp
+        }
+    }
+    // the first number out of place is the result
+    for (var i = 0; i <n; i++) 
+        if(nums[i]!=i+1)
+            return i+1       
+    return n+1
+};
+
+
 
 console.log(
     firstMissingPositive(
-        [1,2,0]
+        [7,8,9,11,12,1,2,5,3,6,10,13]
     )
 )
