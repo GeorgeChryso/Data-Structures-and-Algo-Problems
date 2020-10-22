9a4a32539563e342cc3bab31e83c52e19c6acf06
GeorgeChryso <lsgechrys@gmail.com>
2020-10-22 17:34:48 +0300

    state compression diff

--- topcoder/DP/D&C opt/Turrets and Hills.js
+++ topcoder/DP/D&C opt/Turrets and Hills.js
@@ -34,11 +34,11 @@
 //dp[i][mask][prevmask]
 // is the Maximum turrets I can place on the i-th row using the bitwise mask, while on the previous row I used the bitwise mask prevmask
 
-//obviously this becomes a O(N*(2^m)*(2^m)) solution
+//obviously this becomes a O(N*(2^m)*(2^m)*(2^m)) solution
 
 //dp[i][mask][prevmask]=Math.max(dp[i-1][prevmask][k]+turretcount(mask) ),for each prevmask and k 
 
-let Turrets=A=>{
+var Turrets=A=>{
     let n=A.length,m=A[0].length,result=0
     //first i ll save my matrix as an array of n numbers, 0 for empty places and 1 for hills
     let M=A.map(d=>{
@@ -132,12 +132,235 @@ let Turrets=A=>{
     return result
 }
 
+
+///apparently there's a faster solution
+// State compression=> Compress 2 rows into M integers
+
+// I can encode my 2 previous rows into M base 3 integers
+// and for EACH possible row (O(2^M)), 
+// try all possibilities for the 2 previous rows => O( 3^M)
+// • Three possibilities for the column of the previus two rows
+// • Use base-3 integers to encode
+
+//dp[i][mask] instead holds the max turrets placed till row i
+// if my last 2 rows consist of a number represented by mask in base 3 Int
+
+//dp[i]['2001'] is when  i-1:    0110
+//                         i:    0001
+
+
+// this will reduce the complexity to O(N* 2^M * 3^M )
+// Because I m using 1 number for 2 rows
+// base 3
+// 
+/*
+
+    so  0 maps to  1   
+                   0
+
+        1 maps to  0                so 2010=>  0101
+                   1                           0010        
+                                    and 2010 is invalid btw cos on the first row, 2 turrets are too close
+    and 2 maps to 0
+                  0
+
+
+  State compression but implementation heavy  problem
+*/
+
+// O(n*2^m*3^m) runtime , O(n*m)
+var TurretsNaiveDP=A=>{
+    let n=A.length,m=A[0].length,result=0
+    //first i ll save my matrix as an array of n numbers, 0 for empty places and 1 for hills
+    let M=A.map(d=>{
+        let mask=0,len=d.length
+        for (let i = 0; i < len; i++) 
+            if(d[i]==='H')
+                mask|=(1<<(len-1-i))
+        return mask
+    })
+    //checks if the current choice of mask doesnt place any turrets 
+    // on hills of the current row, and all turrets have at least 2 spaces between them
+    let isValidMask=(mask,row)=>{
+        // turret and hill overlap
+        if(mask&M[row])
+            return false
+        //2spaces between each one
+        while(mask&1==0)
+            mask>>=1
+        let spaces=-1
+        while(mask){
+            if(mask&1){
+                if(spaces>0)
+                    return false
+                spaces=2
+            }
+            else
+                spaces--
+            mask>>=1
+        }
+        return true
+    }
+
+    let countbits=mask=>{
+        let count=0 ,m=mask
+        while(mask)
+            count+=(mask&1),
+            mask>>=1
+        return count
+    }
+    if(n==1){//just 1 row edge case
+        for (let mask = 0; mask < (1<<m); mask++) 
+            if(isValidMask(mask,0))
+                result=Math.max(result,countbits(mask))           
+        return result
+    }
+
+
+    //basecases, any mask for the 2 first rows
+    // the first two rows can be filled wif anyfin as long as its valid
+    let isValidMask2D=(mask,currow)=>{
+        //check if every 1 has a distance of 2 from every other 1
+        // and 0 from the next 0
+        mask=mask.toString(3)
+        let l=mask.length
+        for (let i = 0; i < m-l; i++) 
+            mask='0'+mask    // fill in the zeroes ommited at the beginning of the mask
+        //test for the validity of the mask itself
+        let regex=RegExp(/((00)|(11)|(101)|(121)|(010)|(020))/,'g')
+        if(regex.test(mask))
+            return false
+        // check if it overlaps with any HILLS 
+        for (let i = 0; i < m; i++) {
+            let char=mask[i]
+            if(char==='1'){
+                if(M[currow]&(1<<(m-1-i)))
+                    return false
+            }   
+            else if(char=='0'){
+                if(M[currow-1]&(1<<(m-1-i)))
+                    return false
+            }         
+        }
+        return true
+    }
+    let countbits2d=mask=>{
+        let l=mask.length
+        for (let i = 0; i < m-l; i++) 
+            mask='0'+mask 
+        return mask.split('').reduce((a,c)=>a+(c=='2'?0:1),0)
+    }
+
+
+
+    //checks if prevrows can be the 2 previous rows fo curmask
+    let createTuple=(prevrows,currmask)=>{
+        prevrows=prevrows.toString(3)
+        
+        let l=prevrows.length
+        for (let i = 0; i < m-l; i++) 
+            prevrows='0'+prevrows 
+        //create the res
+        let res=''
+        for (let i = 0; i < m; i++) {
+            let ele=prevrows[i]
+            //check for 0/1 of prevrows
+            // being on the same col as a turned on bit
+            // of currmask            
+            if((currmask&(1<<(m-1-i)))&&ele!=='2')
+                return -1
+            
+            if(currmask&(1<<(m-1-i)))
+                res=res+'1'
+            else{
+                if(ele!='1')
+                    res=res+'2'
+                else
+                    res=res+'0'
+            }            
+        }
+        //return the integer base 3 representation of the new last 2 rows
+        return parseInt(res,3)
+    }
+
+    //for each row, create a memo validMask2dRow[i] which basically 
+    // contains all the possible previous 2 rows that are valid for this row base 3
+    let validmask2dRow=[...Array(n)].map(d=>[...Array(3**m)].map(d=>false))
+    // O(n*3^m*m) to fill it
+    for (let i = 1; i < n; i++) 
+        for (let mask = 0; mask < 3**m; mask++)
+            if(isValidMask2D(mask,i))
+                validmask2dRow[i][mask]=true
+    
+    // console.log(validmask2dRow[1].map((d,i)=>[d,i]).filter(([a,b])=>a)
+    // )
+    //for each row, create also a validMask1dRow[i] which contains all the possible
+    // masks base 2 for that row
+    let validMask1dRow=[...Array(n)].map(d=>[...Array(1<<m)].map(d=>false))
+    // O(n*2^m*m)runtime to fill it
+    for (let i = 0; i < n; i++) 
+        for (let mask = 0; mask < (1<<m); mask++)
+            if(isValidMask(mask,i))
+                validMask1dRow[i][mask]=true
+
+    //count bits for each mask base 2
+    let count=[...Array(1<<m)] 
+    for (let mask = 0; mask < (1<<m); mask++)
+        count[mask]=countbits(mask)
+    //for each possible combination of a row in base 2 and 2 previous rows in base 3
+    // create a isValid3rows[prev2_base_3][curr_base_2] which tells me if it can be a valid
+    // 3 consecutive rows
+    // O(m*2^M*3^N) runtime O(3**M*2**M) space
+    let isValid3rows=[...Array(3**m)].map(d=>[...Array(1<<m)].map(d=>-1))
+    for (let mask = 0; mask < 3**m; mask++)
+        for (let curmask = 0; curmask < (1<<m); curmask++)
+        // -1 means it's not possible for this to be consecutive
+        // else the number is the new base 3 representation of the last 2 rows
+            isValid3rows[mask][curmask]=createTuple(mask,curmask)
+
+
+    //dp part
+    //initialize my matrix, N*3^M cos it saves the last 2 compressed rows
+    let dp=[...Array(n)].map(d=>[...Array(3**m)].map(d=>-1))
+
+    //basecase
+    for (let mask = 0; mask < (3**m); mask++)
+        if(validmask2dRow[1][mask])
+            dp[1][mask]=countbits2d(mask.toString(3)),result=Math.max(result,dp[1][mask])
+        
+
+
+    for(let i=2;i<n;i++){
+        //try all cases for the prev 2 rows
+        for (let mask = 0; mask < (3**m); mask++) {
+            if(!validmask2dRow[i-1][mask])
+                continue
+            for (let curmask = 0; curmask < (1<<m); curmask++) {
+                if(!validMask1dRow[i][curmask])
+                    continue
+                if(isValid3rows[mask][curmask]<0)
+                    continue
+                let cur2rows=isValid3rows[mask][curmask] //turned into a number from base 3
+                dp[i][cur2rows]=Math.max(dp[i][cur2rows],dp[i-1][mask]+count[curmask])       
+                result=Math.max(result,dp[i][cur2rows])         
+            }
+        }
+    }
+    return result
+}
+
+let b3=n=>{
+    console.log(n.toString(3),parseInt(n.toString(3),3))
+}
+
+
 console.log(
     Turrets(
         ['PP',
         'PH']
     )
 )
+
 
 console.log(
     Turrets(
@@ -148,6 +371,7 @@ console.log(
         'PHHP']
     )
 )
+
 
 
 let testcase=
@@ -242,7 +466,7 @@ let testcase=
     'PPPPPPPPPP',
     'PPPPPPPPPP',
     'PPPPPPPPPP',
-     'PPPPPPPPPP',
+    'PPPPPPPPPP',
     'PPPPPPPPPP',
     'PPPPPPPPPP',
     'PPPPPPPPPP',
