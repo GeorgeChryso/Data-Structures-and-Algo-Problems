8ace8ebe25dbf6aaac3a542e4e0751aef10d8861
GeorgeChryso <lsgechrys@gmail.com>
2020-11-01 20:07:41 +0200

    Pq approach added

--- 1642. Furthest Building You Can Reach.js
+++ 1642. Furthest Building You Can Reach.js
@@ -23,7 +23,6 @@
 
 // Intuition:
 // Binary search for the answer, if he cant get to k-th city, he surely cant get to k+1th etc
-
 // O (N* logN* logN)
 var furthestBuilding = function(heights, bricks, ladders) {
     // First, we can view the array as an array of deltas,
@@ -61,10 +60,143 @@ var furthestBuilding = function(heights, bricks, ladders) {
         else
             hi=mid-1
     }
-    return lo
+    return res
 };
+
+
+
+class minBinaryHeap{
+    constructor(){
+        this.heap=[]
+        //this is the simplest comparator between a and b and returns 
+        // a positive number if a >b
+        // a negative number if a < b
+        // or 0 when a ===b, 
+        // adjusting this for every situation will allow me to use heaps outside of the 
+        // just numbers context
+        this.comparator=(a,b)=>a-b
+    }
+
+    hasParent=index=>index>=1
+    getParent=(index)=>this.heap[Math.floor((index-1)/2)]
+    
+    hasLeft=(index)=>2*index+1<=this.heap.length-1
+    getLeftChild=(index)=>this.heap[2*index+1]
+    
+    hasRight=index=>2*index+2<=this.heap.length-1
+    getRightChild=(index)=> this.heap[2*index+2]
+    
+    length=()=>this.heap.length
+
+    peek=()=>this.heap[0]
+
+    push(element){
+        this.heap.push(element)
+        //this element is pushed on the rightmost node of the lowest level
+        // and needs  to be bubbled up accordingly
+        this.bubbleUp(this.heap.length-1)
+    }
+
+    bubbleUp(index){
+        //if there is a parent with a bigger priority, switch places with my index
+        while(this.hasParent(index)&&(this.comparator(this.heap[index],this.getParent(index))<0)){
+            //swap the two elements until the Invariant is reached
+            this.swap(index,Math.floor((index-1)/2))
+            // and update the new index to be its parent's index, since u switched the items
+            index=Math.floor((index-1)/2)
+        }
+    }
+
+    //get the highest(lowest) priority element
+    poll(){
+        if(this.length()==1)return this.heap.pop()
+
+        let result=this.heap[0]
+        this.heap[0]=this.heap.pop()
+        this.bubbleDown(0)
+        return result
+    }
+    
+    //after every poll, the new item on place 0 needs to be bubbled down to its correct position
+    bubbleDown(index){
+        if(this.length()<=1)return
+
+        while(this.hasLeft(index)&&(this.comparator(this.heap[index],this.getLeftChild(index))>0||(this.hasRight(index)&&this.comparator(this.heap[index],this.getRightChild(index))>0) )){
+
+            //if there is no right child, swap with the left
+            if(!this.hasRight(index)){
+                this.swap(index,index*2+1)
+                index=index*2+1
+            }
+            else{
+                // if the left child is less than or equal to the right child, choos the left
+                if(this.comparator(this.getLeftChild(index),this.getRightChild(index))<=0){
+                    //and swap
+                    this.swap(index,index*2+1)
+                    index=index*2+1
+                }
+                // else choose the right child
+                else {
+                    //and swap
+                  this.swap(index,index*2+2)
+                  index=index*2+2
+
+                }
+                
+            }
+        }
+    }
+    swap=(a,b)=>{
+        if(a===b)return
+        let temp=this.heap[b]
+        this.heap[b]=this.heap[a]
+        this.heap[a]=temp
+    }
+}
+// Better approach, maybe a use of a priority queue of length k=ladders, holding the 
+// k biggest elements so far can indicate whether I can travel up to this  point or not
+// I store the total amount of bricks required to get to a point with a simple prefix sum
+// then I hold a window on my pq of length ladders, and keep track of the currentsum of that window
+// the sum indicates the bricks that can be replaced using ladders
+// O(NlogK), where K=available ladders
+var furthestBuilding = function(heights, bricks, ladders) {
+    let deltas=heights.map((d,i)=>{
+        if(i==0 || (heights[i]<=heights[i-1]))
+            return 0
+        return heights[i]-heights[i-1]
+    }),
+        n=deltas.length,prefixSum=[0]
+    for (let i = 0; i <n; i++)
+        prefixSum.push(prefixSum[prefixSum.length-1]+deltas[i])
+
+    let pq=new minBinaryHeap(),currentKsum=0
+    // min heap, for storing the k max elements in reverse order
+    pq.comparator=(a,b)=>a-b
+
+    for (let i = 0; i <n; i++){
+        if(pq.length()<ladders) //maintain a pq of length ladders
+            pq.push(deltas[i]),
+            currentKsum+=deltas[i]
+        else{
+            // An element is found that must be placed on the pq
+            if(deltas[i]>=pq.peek()) 
+                currentKsum-=pq.poll(), //remove the smallest
+                currentKsum+=deltas[i], // add the new
+                pq.push(deltas[i])
+
+            // Is this impossible? The first impossible location means 
+            // that the previous one was the biggest possible. 
+            if(prefixSum[i+1]-currentKsum>bricks)
+                return i-1  //if yes, then the previous was possible
+        }
+    }
+    return n-1 // I can get to every location
+};
+
+
+
 console.log(furthestBuilding(
-    [17,16,5,10,10,14,7],
-    74,
-    6
+    [4,2,7,6,9,14,12],
+    5,
+    1
 ))
